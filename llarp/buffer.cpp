8c3bd25825dc3b87976e9dc0f9d3f5004680c7ea
Jeff <ampernand@gmail.com>
2018-02-01 17:10:47 -0500

    fix on os x

--- llarp/buffer.cpp
+++ llarp/buffer.cpp
@@ -4,7 +4,7 @@
 extern "C" {
 
 size_t llarp_buffer_size_left(llarp_buffer_t *buff) {
-  std::ptrdiff_t diff = buff->cur - buff->base;
+  auto diff = buff->cur - buff->base;
   if (diff < 0) {
     return 0;
   } else if (diff > buff->sz)
