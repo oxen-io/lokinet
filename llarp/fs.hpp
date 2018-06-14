72bc32dcf43c83721fabbbddc1afae31ba3b8dc8
Ryan Tharp <neuro@interx.net>
2018-06-14 00:48:42 -0700

    define PATH_SEP, llvm 3.8 fs shim

--- llarp/fs.hpp
+++ llarp/fs.hpp
@@ -1,21 +1,33 @@
 #ifndef LLARP_FS_HPP
 #define LLARP_FS_HPP
 
-#if(__cplusplus >= 201703L)
-#include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
+#if defined(WIN32) || defined(_WIN32)
+#  define PATH_SEP "\\"
 #else
-
-#error "fs support unimplemented"
-
-#include <string>
-
-namespace fs
-{
-  static std::string Sep = "/";
-  struct path
-  {
-  };
-}
+#  define PATH_SEP "/"
 #endif
+
+#if(__cplusplus >= 201703L)
+  #include <experimental/filesystem>
+  namespace fs = std::experimental::filesystem;
+#else
+  #ifdef __clang__
+    #include "filesystem.h"
+    namespace fs = cpp17::filesystem;
+  #else
+    #error "fs support unimplemented"
+
+    #include <string>
+
+    namespace fs
+    {
+      static std::string Sep = "/";
+      struct path
+      {
+      };
+    }
+
+  #endif
+#endif
+
 #endif
