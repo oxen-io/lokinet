53c5474dc6fe76d4325601ac36b1031d624fa565
Ryan Tharp <neuro@interx.net>
2018-06-21 04:13:40 -0700

    hexDecode helper

--- llarp/encode.cpp
+++ llarp/encode.cpp
@@ -1,21 +1,27 @@
 #include <llarp/encode.hpp>
+#include <stdexcept>
 
-int char2int(char input)
+namespace llarp
 {
-  if(input >= '0' && input <= '9')
-    return input - '0';
-  if(input >= 'A' && input <= 'F')
-    return input - 'A' + 10;
-  if(input >= 'a' && input <= 'f')
-    return input - 'a' + 10;
-  throw std::invalid_argument("Invalid input string");
-}
 
-void HexDecode(const char* src, uint8_t* target)
-{
-  while(*src && src[1])
+  int char2int(char input)
   {
-    *(target++) = char2int(*src)*16 + char2int(src[1]);
-    src += 2;
+    if(input >= '0' && input <= '9')
+      return input - '0';
+    if(input >= 'A' && input <= 'F')
+      return input - 'A' + 10;
+    if(input >= 'a' && input <= 'f')
+      return input - 'a' + 10;
+    throw std::invalid_argument("Invalid input string");
   }
+
+  void HexDecode(const char* src, uint8_t* target)
+  {
+    while(*src && src[1])
+    {
+      *(target++) = char2int(*src)*16 + char2int(src[1]);
+      src += 2;
+    }
+  }
+
 }
