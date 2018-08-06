863ef1f71e420c50caf207dcac54942c4cba95a9
Jeff Becker <jeff@i2p.rocks>
2018-08-06 10:06:00 +1000

    more android code

--- jni/lokinet_android.cpp
+++ jni/lokinet_android.cpp
@@ -3,17 +3,16 @@
 #include <jni.h>
 #include <llarp.h>
 #include <signal.h>
+#include <memory>
 #include <thread>
 
-struct AndroidMain 
+struct AndroidMain
 {
-  llarp_main * m_impl = nullptr;
-  std::thread * m_thread = nullptr;
+  llarp_main* m_impl    = nullptr;
+  std::thread* m_thread = nullptr;
 
   ~AndroidMain()
   {
-    if(m_impl)
-      llarp_main_free(m_impl);
     if(m_thread)
     {
       m_thread->join();
@@ -21,34 +20,45 @@ struct AndroidMain
     }
   }
 
-  void Start()
+  void
+  Start()
   {
     if(m_impl || m_thread)
       return;
-    m_impl = llarp_main_init("daemon.ini", true);
+    m_impl   = llarp_main_init("daemon.ini", true);
     m_thread = new std::thread(std::bind(&AndroidMain::Run, this));
   }
 
-  void Run()
+  void
+  Run()
   {
     llarp_main_run(m_impl);
+    llarp_main_free(m_impl);
   }
 
-  void Stop()
+  void
+  Stop()
   {
     llarp_main_signal(m_impl, SIGINT);
   }
 
-  typedef std::unique_ptr<AndroidMain> Ptr;
+  typedef std::unique_ptr< AndroidMain > Ptr;
 
-  static std::atomic<Ptr> daemon = std::make_unique<AndroidMain>();
+  static std::atomic< Ptr > daemon = std::make_unique< AndroidMain >();
 }
 
 extern "C"
 {
+  JNIEXPORT jstring JNICALL
+      Java_network_loki_lokinet_Lokinet_1JNI_getABICompiledWith(JNIEnv * env,
+                                                                jclass)
+  {
+    // TODO: fixme
+    return env->NewUTFString("android");
+  }
 
-JNIEXPORT jstring JNICALL Java_network_loki_lokinet_Lokinet_1JNI_startLokinet
-  (JNIEnv * env, jclass jcl)
+  JNIEXPORT jstring JNICALL Java_network_loki_lokinet_Lokinet_1JNI_startLokinet(
+      JNIEnv * env, jclass jcl)
   {
     if(AndroidMain::daemon->Running())
       return env->NewUTFString("already running");
@@ -56,4 +66,18 @@ JNIEXPORT jstring JNICALL Java_network_loki_lokinet_Lokinet_1JNI_startLokinet
     return env->NewUTFString("ok");
   }
 
+  JNIEXPORT void JNICALL Java_network_loki_lokinet_Lokinet_1JNI_stopLokinet(
+      JNIEnv * env, jclass)
+  {
+    if(AndroidMain::daemon->Running())
+    {
+      AndroidMain::daemon->Stop();
+    }
+  }
+
+  JNIEXPORT void JNICALL
+  Java_network_loki_lokinet_Lokinet_1JNI_onNetworkStateChanged(JNIEnv*, jclass,
+                                                               jboolean)
+  {
+  }
 }
