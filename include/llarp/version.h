331a13ce0b3cc60152da6d9b6706aa39890dcb6e
Jeff Becker <jeff@i2p.rocks>
2018-09-08 12:32:58 -0400

    bump version to 0.2.1

--- include/llarp/version.h
+++ include/llarp/version.h
@@ -6,11 +6,11 @@
 #endif
 
 #ifndef LLARP_VERSION_MIN
-#define LLARP_VERSION_MIN "1"
+#define LLARP_VERSION_MIN "2"
 #endif
 
 #ifndef LLARP_VERSION_PATCH
-#define LLARP_VERSION_PATCH "0"
+#define LLARP_VERSION_PATCH "1"
 #endif
 
 #ifndef LLARP_VERSION_NUM
