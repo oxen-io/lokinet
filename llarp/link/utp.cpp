ce03801a51e58744deb7b3465f1af14f502931d4
Jeff Becker <jeff@i2p.rocks>
2018-09-23 07:29:41 -0400

    close socket in destructor

--- llarp/link/utp.cpp
+++ llarp/link/utp.cpp
@@ -736,8 +736,9 @@ namespace llarp
     {
       if(sock)
       {
+        utp_shutdown(sock, SHUT_RDWR);
+        utp_close(sock);
         utp_set_userdata(sock, nullptr);
-        sock = nullptr;
       }
     }
 
