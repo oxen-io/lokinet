79d61dedc3514593a51ca0c39ff69889b696033e
Ryan Tharp <neuro@interx.net>
2018-09-29 03:28:55 -0700

    use new llarp::Addr based init

--- llarp/handlers/tun.cpp
+++ llarp/handlers/tun.cpp
@@ -245,8 +245,11 @@ namespace llarp
 #ifndef _WIN32
       m_TunSetupResult.set_value(result);
 #endif
+      llarp::Addr dnsd_sockaddr(127, 0, 0, 1, 53);
+      llarp::Addr dnsc_sockaddr(8, 8, 8, 8, 53);
+      llarp::LogInfo("TunDNS set up ", dnsd_sockaddr, " to ", dnsc_sockaddr);
       if(!llarp_dnsd_init(&this->dnsd, EndpointLogic(), EndpointNetLoop(),
-                          tunif.ifname, 53, "8.8.8.8", 53))
+                          dnsd_sockaddr, dnsc_sockaddr))
       {
         llarp::LogError("Couldnt init dns daemon");
       }
