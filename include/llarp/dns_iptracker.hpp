f032a83e37094d7a24bc61f79c711891ce2cd37e
Ryan Tharp <neuro@interx.net>
2018-10-03 03:52:00 -0700

    use llarp::Addr to store local private ip

--- include/llarp/dns_iptracker.hpp
+++ include/llarp/dns_iptracker.hpp
@@ -11,8 +11,7 @@
 // neither, it's a result set row
 struct dns_pointer
 {
-  // FIXME change to llarp::Addr
-  struct sockaddr *hostResult;
+  llarp::Addr hostResult;
   llarp::service::Address b32addr;
   // we could store the timeout at which we expect it to be available
   // or a list of pending requests for it
@@ -22,7 +21,6 @@ struct ip_range
 {
   uint8_t octet2;
   uint8_t octet3;
-  // FIXME: we're not consecutive
   uint8_t left;
   std::unordered_map< uint8_t, dns_pointer * > used;
 };
