da9850f0e30eb4bac89395037f8d392d998ec507
Ryan Tharp <neuro@interx.net>
2018-10-11 04:51:13 -0700

    start of dns unit tests

---
+++ test/test_dnsc_unit.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+#include <llarp.h> // for llarp_main_init
+#include <llarp/logic.h> // for threadpool/llarp_logic
+#include "llarp/net.hpp"  // for llarp::Addr
+#include "llarp/dnsc.hpp"
