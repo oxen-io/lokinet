7039d70aa52226b8e84a3040300e010a8c31488f
Ryan Tharp <neuro@interx.net>
2018-11-18 15:47:53 -0800

    clean up

--- include/llarp/dns.h
+++ include/llarp/dns.h
@@ -1,15 +1,6 @@
 #ifndef LLARP_DNS_H_
 #define LLARP_DNS_H_
 
-//#include <llarp/ev.h>   // for sockaadr
-//#include <sys/types.h>  // for uint & ssize_t
-
-// all uint should have been removed in favor of uint_16t
-/* non-cygnus does not have this type */
-//#ifdef _WIN32
-//#define uint UINT
-//#endif
-
 #ifdef __cplusplus
 extern "C"
 {
