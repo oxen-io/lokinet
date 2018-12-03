90aca1da06d16e2531e2cbb25730421e00c385e7
despair <despair@rvx86.net>
2018-12-02 22:35:49 -0600

    check in win32 ui code

---
+++ ui-win32/UIMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace lokivpn
+{
+    public partial class UI_Main_Frame : Form
+    {
+        public UI_Main_Frame()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConfigProfile_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("not implemented yet", "error", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+    }
+}
