e29be3966deb8d773e683fc13827c63bea22016c
Lilac <lilac@dissent.cloud>
2018-12-10 00:51:00 +1000

    Cleanup and add development files
    Cleaned up the formula, added comments.
    
    Now installs the libraries and development headers instead of just the binaries.

--- homebrew/Formula/loki-network.rb
+++ homebrew/Formula/loki-network.rb
@@ -1,43 +1,61 @@
 class LokiNetwork < Formula
-  desc "Lokinet is a private, decentralized and Market based Sybil resistant overlay network for the internet"
+  desc "Private, decentralized, and market-based overlay for the internet"
   homepage "https://loki.network"
   url "https://github.com/loki-project/loki-network/archive/v0.3.1.tar.gz"
   sha256 "a745cd156fd08be575f0e9f2fd56f4d6cde896363103c6f49499c90451021b34"
-  
-  depends_on "cmake" => :build
-  depends_on "rapidjson" => :recommended
-  depends_on "abyss" => :optional
-  depends_on "ninja" => :build
-  option "with-jsonrpc" "Build with JSON-RPC support"
-  option "with-avx2" "Build with AVX2 support"
-  option "with-netns" "Build with network namespace support"
+  head "https://github.com/loki-project/loki-network.git"
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "ninja" => [:build, :test]
+  depends_on "abyss"
+  depends_on "rapidjson"
 
   def install
-    ENV["USING_CLANG"] = "ON"
-
-    # build time options
-    ENV["USE_LIBABYSS"] = "ON" if build.with? "abyss"
-    ENV["USE_AVX2"] = "ON" if build.with? "avx2"
-    ENV["USE_NETNS"] = "ON" if build.with? "netns"
-
-    system "mkdir", "-p", "#{buildpath}/build"
-    system "cd", "#{buildpath}/build"
-
-    cmake_args = *std_cmake_args
-    cmake_args << "-GNinja"
-
-    system "cmake", "-B", "#{buildpath}/build", "-S", "#{buildpath}", *cmake_args
+    # set some build time options
+    ENV["USE_AVX2"] = "1"
+    ENV["USE_LIBABYSS"] = "1"
+    ENV["USE_JSONRPC"] = "1"
+    # make a subdirectory to store build outputs
+    mkdir "#{buildpath}/build"
+    # generate ninja build system files
+    system "cmake", "-GNinja", "-B", "#{buildpath}/build", "-S", buildpath.to_s, *std_cmake_args
+    # build everything
     system "ninja", "-C", "#{buildpath}/build"
-    system "ninja", "-C", "#{buildpath}/build", "install"
-
-    bin.install "#{buildpath}/build/lokinet"
-    bin.install "#{buildpath}/lokinet-bootstrap"
-    libexec.install "#{buildpath}/build/dns"
-    libexec.install "#{buildpath}/build/llarpc"
-    libexec.install "#{buildpath}/build/rcutil"
+    cd "#{buildpath}/build"
+    system "ninja"
+    # install binaries
+    libexec.install [
+      "#{buildpath}/build/lokinet",
+      "#{buildpath}/lokinet-bootstrap",
+      "#{buildpath}/build/dns",
+      "#{buildpath}/build/llarpc",
+      "#{buildpath}/build/rcutil",
+    ]
+    # symlink main binaries into #{prefix}/bin
+    link "#{libexec}/lokinet", "#{bin}/lokinet"
+    link "#{libexec}/lokinet-bootstrap", "#{bin}/lokinet-bootstrap"
+    # install library headers
+    include.install [
+      "#{buildpath}/include/llarp.h",
+      "#{buildpath}/include/llarp.hpp",
+      "#{buildpath}/include/tuntap.h",
+      "#{buildpath}/include/utp.h",
+      "#{buildpath}/include/utp_types.h",
+    ]
+    include.install Dir[
+      "#{buildpath}/include/llarp",
+      "#{buildpath}/include/tl",
+    ]
+    # install libraries
+    lib.install [
+      "#{buildpath}/build/liblokinet-cryptography.a",
+      "#{buildpath}/build/liblokinet-platform.a",
+      "#{buildpath}/build/liblokinet-static.a",
+    ]
   end
 
   test do
+    # run lokinet's self-test binary
     system "#{buildpath}/build/testAll"
   end
 end
