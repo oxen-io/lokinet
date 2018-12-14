42e4b8661cd7c04a8a3f85d0c9efd57eb6045922
Jeff Becker <jeff@i2p.rocks>
2018-12-14 08:51:00 -0500

    try fix for freebsd

--- llarp/ev_kqueue.hpp
+++ llarp/ev_kqueue.hpp
@@ -228,7 +228,6 @@ namespace llarp
       return -1;
     }
 
-#ifdef __APPLE__
     virtual ssize_t
     do_write(void* buf, size_t sz)
     {
@@ -241,7 +240,6 @@ namespace llarp
       vecs[1].iov_len  = sz;
       return writev(fd, vecs, 2);
     }
-#endif
 
     void
     flush_write()
