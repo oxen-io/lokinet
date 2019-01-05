535c65ca5228a49b12cee2831fe84cf05662f72a
Michael <michael@loki.network>
2019-01-05 00:49:06 +0000

    Fix RCTest dependency ordering

--- llarp/rpc.cpp
+++ llarp/rpc.cpp
@@ -90,7 +90,7 @@ namespace llarp
           if(key_itr->IsString())
           {
             keys.emplace_back();
-            if(!HexDecode(key_itr->GetString(), keys.back(), PUBKEYSIZE))
+            if(!HexDecode(key_itr->GetString(), keys.back().begin(), decltype(keys)::value_type::SIZE))
             {
               keys.pop_back();
             }
