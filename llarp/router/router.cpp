e94a7b88e75eb3a495164398652e2fa770794344
Ryan Tharp <neuro@interx.net>
2019-01-29 03:49:02 -0800

    fix typo in log message

--- llarp/router/router.cpp
+++ llarp/router/router.cpp
@@ -196,7 +196,7 @@ namespace llarp
   Router::OnSessionEstablished(llarp::RouterContact rc)
   {
     async_verify_RC(rc, nullptr);
-    llarp::LogInfo("session with ", rc.pubkey, "established");
+    llarp::LogInfo("session with ", rc.pubkey, " established");
   }
 
   Router::Router(struct llarp_threadpool *_tp, struct llarp_ev_loop *_netloop,
