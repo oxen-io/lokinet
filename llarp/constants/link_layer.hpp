df0120f161912904a788fbccf3c8dd4abad0be9e
Jeff Becker <jeff@i2p.rocks>
2019-03-04 11:04:08 -0500

    fix limit

--- llarp/constants/link_layer.hpp
+++ llarp/constants/link_layer.hpp
@@ -5,5 +5,5 @@
 #include <stdlib.h>
 
 constexpr size_t MAX_LINK_MSG_SIZE                = 8192;
-constexpr llarp_time_t DefaultLinkSessionLifetime = 10 * 1000;
+constexpr llarp_time_t DefaultLinkSessionLifetime = 60 * 1000;
 #endif
