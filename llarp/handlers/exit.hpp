2eb6d8a3aed03ea38af14763f55066385333f600
Jeff Becker <jeff@i2p.rocks>
2019-03-20 12:41:48 -0400

    fix name

--- llarp/handlers/exit.hpp
+++ llarp/handlers/exit.hpp
@@ -126,7 +126,7 @@ namespace llarp
       KickIdentOffExit(const PubKey& pk);
 
       AbstractRouter* m_Router;
-      service::dns::Proxy m_Resolver;
+      dns::Proxy m_Resolver;
       bool m_ShouldInitTun;
       std::string m_Name;
       bool m_PermitExit;
