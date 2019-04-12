fdfd669edc1529d7fd5d22d44cf934254073a213
Niels Andriesse <andriesseniels@gmail.com>
2019-04-12 16:22:03 +1000

    WIP

--- ios/Loki Network/ViewController.swift
+++ ios/Loki Network/ViewController.swift
@@ -1,9 +1,28 @@
 import UIKit
+import NetworkExtension
 
 class ViewController : UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Start tunnel
+        let tunnelProviderManager = NETunnelProviderManager()
+        let prtcl = NETunnelProviderProtocol()
+        prtcl.providerBundleIdentifier = Bundle.main.bundleIdentifier!
+        prtcl.serverAddress = "172.16.10.1"
+        tunnelProviderManager.protocolConfiguration = prtcl
+        tunnelProviderManager.saveToPreferences { error in
+            if let error = error {
+                print(error)
+            } else {
+                do {
+                    try tunnelProviderManager.connection.startVPNTunnel()
+                } catch let error {
+                    print(error)
+                }
+            }
+        }
+        // Start daemon
         let daemon = Daemon.shared
         daemon.configure(isDebuggingEnabled: true) { [weak daemon] context in
             daemon?.run(with: context)
--- ios/Packet Tunnel Provider/PacketTunnelProvider.swift
+++ ios/Packet Tunnel Provider/PacketTunnelProvider.swift
@@ -2,11 +2,11 @@ import NetworkExtension
 
 final class PacketTunnelProvider : NEPacketTunnelProvider {
     
-    override func startTunnel(options: [String : NSObject]? = nil, completionHandler: @escaping (Error?) -> Void) {
-        // TODO: Implement
+    override func startTunnel(options: [String:NSObject]? = nil, completionHandler: @escaping (Error?) -> Void) {
+        print("startTunnel(...)")
     }
     
     override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
-        // TODO: Implement
+        print("stopTunnel(...)")
     }
 }
