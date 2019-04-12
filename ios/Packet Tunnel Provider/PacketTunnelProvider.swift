69e737646d9f8c03c029d63b46ae5c518e3f6523
Niels Andriesse <andriesseniels@gmail.com>
2019-04-12 15:23:53 +1000

    Create packet tunnel provider skeleton

---
+++ ios/Packet Tunnel Provider/PacketTunnelProvider.swift
@@ -0,0 +1,12 @@
+import NetworkExtension
+
+final class PacketTunnelProvider : NEPacketTunnelProvider {
+    
+    override func startTunnel(options: [String : NSObject]? = nil, completionHandler: @escaping (Error?) -> Void) {
+        // TODO: Implement
+    }
+    
+    override func stopTunnel(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
+        // TODO: Implement
+    }
+}
