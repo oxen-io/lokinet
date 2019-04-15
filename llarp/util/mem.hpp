797512ddf48dd8dc8d8eebc440bf2cb192b70fde
Michael <michael@loki.network>
2019-04-15 02:00:22 +0100

    More minor windows fixes

--- llarp/util/mem.hpp
+++ llarp/util/mem.hpp
@@ -93,15 +93,4 @@ namespace llarp
 
 }  // namespace llarp
 
-#if __cplusplus < 201402L
-namespace std
-{
-  template < typename T, typename... Args >
-  std::unique_ptr< T >
-  make_unique(Args &&... args)
-  {
-    return std::unique_ptr< T >(new T(std::forward< Args >(args)...));
-  }
-}  // namespace std
-#endif
 #endif
