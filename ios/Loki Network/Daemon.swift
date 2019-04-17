139858f2af1450088384539677f38677f8443bd1
Niels Andriesse <andriesseniels@gmail.com>
2019-04-17 12:20:19 +1000

    Fix permission issue

--- ios/Loki Network/Daemon.swift
+++ ios/Loki Network/Daemon.swift
@@ -14,16 +14,16 @@ final class Daemon {
     // MARK: Configuration
     func configure(isDebuggingEnabled: Bool, completionHandler: @escaping (LLARPContext) -> Void) { // TODO: Handle errors
         // Prepare
-        let bundlePath = Bundle.main.bundlePath
+        let directoryPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.path
         // Enable debugging mode if needed
         if isDebuggingEnabled { _llarp_enable_debug_mode() }
         // Generate configuration file
-        let configurationFilePath = bundlePath + "/" + "liblokinet-configuration.ini"
-        _llarp_ensure_config(configurationFilePath, bundlePath, true, false)
+        let configurationFilePath = directoryPath + "/" + "liblokinet-configuration.ini"
+        _llarp_ensure_config(configurationFilePath, directoryPath, true, false)
         // Download bootstrap file
         let remoteBootstrapFilePath = "https://i2p.rocks/i2procks.signed"
         let downloadTask = URLSession.shared.dataTask(with: URL(string: remoteBootstrapFilePath)!) { [weak self] data, _, _ in
-            let localBootstrapFilePath = bundlePath + "/" + "bootstrap.signed"
+            let localBootstrapFilePath = directoryPath + "/" + "bootstrap.signed"
             try! data!.write(to: URL(fileURLWithPath: localBootstrapFilePath))
             // Perform main setup
             let context = _llarp_main_init(configurationFilePath, false)!
