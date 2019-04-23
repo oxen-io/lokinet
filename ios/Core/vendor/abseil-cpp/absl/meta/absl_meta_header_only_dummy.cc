dc1d7e911404129923af5136e8071da31662c1ce
Niels Andriesse <andriesseniels@gmail.com>
2019-04-23 16:50:40 +1000

    Ditch build folder entirely

--- ios/Core/vendor/abseil-cpp/absl/meta/absl_meta_header_only_dummy.cc
+++
@@ -1,8 +0,0 @@
-/* generated file for header-only cmake target */
-
-      namespace absl {
-
-       // single meaningless symbol
-       void absl_meta__header_fakesym() {}
-      }  // namespace absl
-      
--- ios/Core/vendor/abseil-cpp/absl/types/absl_span_header_only_dummy.cc
+++
@@ -1,8 +0,0 @@
-/* generated file for header-only cmake target */
-
-      namespace absl {
-
-       // single meaningless symbol
-       void absl_span__header_fakesym() {}
-      }  // namespace absl
-      
