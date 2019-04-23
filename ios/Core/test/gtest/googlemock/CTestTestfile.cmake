0c204e3e85277e5ab0cae685a5e8135290c8fd65
Niels Andriesse <andriesseniels@gmail.com>
2019-04-23 11:04:52 +1000

    Remove more build artifacts

--- ios/Core/test/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/niels/Code/loki-network/test/gtest/googlemock
-# Build directory: /Users/niels/Code/loki-network/ios/Core/test/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- ios/Core/test/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/niels/Code/loki-network/test/gtest/googletest
-# Build directory: /Users/niels/Code/loki-network/ios/Core/test/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
