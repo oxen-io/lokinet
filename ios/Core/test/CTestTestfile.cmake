e9c46f8d991f324791a9dc82b1ebe6b14ae06f4e
Niels Andriesse <andriesseniels@gmail.com>
2019-04-23 11:07:20 +1000

    Remove even more build artifacts

--- ios/Core/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/niels/Code/loki-network/test
-# Build directory: /Users/niels/Code/loki-network/ios/Core/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
