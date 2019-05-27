6f3465f54684a9d162509e73877cea583fbf862e
Jeff Becker <jeff@i2p.rocks>
2019-05-27 07:48:44 -0400

    add missing lines

--- contrib/munin/lokinet-munin.py
+++ contrib/munin/lokinet-munin.py
@@ -23,6 +23,7 @@ def exit_sessions_main(exe):
         print("graph_vlabel sessions")
         print("graph_category network")
         print("graph_info This graph shows the number of exit sessions on a lokinet exit")
+        print("{}.label lokinet exit".format(exe))
         print("{}.sessions.info Number of exit sessions".format(exe))
         print("{}.sessions.label sessions".format(exe))
     else:
@@ -41,6 +42,7 @@ def peers_main(exe):
         print("graph_vlabel peers")
         print("graph_category network")
         print("graph_info This graph shows the number of node to node sessions of this lokinet router")
+        print("{}.label lokinet peers".format(exe))
         print("{}.outbound.info Number of outbound lokinet peers".format(exe))
         print("{}.inbound.info Number of inbound lokinet peers".format(exe))
         print("{}.outbound.label outbound peers".format(exe))
