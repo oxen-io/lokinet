068fec82fbe57aafc2960a80b3fbe5db7ab8b737
Jeff Becker <jeff@i2p.rocks>
2019-05-28 10:06:01 -0400

    set lifetime of paths

--- llarp/messages/relay_commit.hpp
+++ llarp/messages/relay_commit.hpp
@@ -29,7 +29,7 @@ namespace llarp
     std::unique_ptr< RouterContact > nextRC;
     std::unique_ptr< PoW > work;
     uint64_t version  = 0;
-    uint64_t lifetime = 0;
+    uint64_t lifetime = path::default_lifetime;
 
     bool
     BDecode(llarp_buffer_t *buf);
