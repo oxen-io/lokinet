a0c8c9fda95e00dad33dd60731df87b1aed3a9e6
Niels Andriesse <andriesseniels@gmail.com>
2019-07-09 16:15:12 +1000

    Fix display name

--- ios/PacketTunnelProvider/LKMessage.swift
+++ ios/PacketTunnelProvider/LKMessage.swift
@@ -1,12 +1,14 @@
 import Foundation
 
 struct LKMessage {
+    private let headerSizeInBytes = 2
     
     enum Kind : String {
         case requestSession, acceptSession, rejectSession
     }
     
     init(from data: Data) {
+        let payloadAsData = data[headerSizeInBytes...data.endIndex]
         
     }
 }
