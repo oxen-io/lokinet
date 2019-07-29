60fbeca9d461f70cfddadb8695b497d9dcd554b3
Jeff Becker <jeff@i2p.rocks>
2019-07-29 12:43:24 -0400

    const correctness

--- llarp/path/pathbuilder.cpp
+++ llarp/path/pathbuilder.cpp
@@ -132,7 +132,7 @@ namespace llarp
   {
     if(!ctx->pathset->IsStopped())
     {
-      RouterID remote         = ctx->path->Upstream();
+      const RouterID remote   = ctx->path->Upstream();
       const ILinkMessage* msg = &ctx->LRCM;
       if(ctx->router->SendToOrQueue(remote, msg))
       {
