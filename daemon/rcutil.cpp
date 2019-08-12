0a1620aff22510cc0b0ee1c4168d8777bf6d280a
Michael <michael@loki.network>
2019-08-12 22:10:07 +0000

    make format

--- daemon/rcutil.cpp
+++ daemon/rcutil.cpp
@@ -6,40 +6,41 @@
 #include <string>
 #include <vector>
 
-bool dumpRc(const std::vector<std::string>& files, bool json)
+bool
+dumpRc(const std::vector< std::string >& files, bool json)
 {
-    nlohmann::json result;
-    for(const auto& file : files)
+  nlohmann::json result;
+  for(const auto& file : files)
+  {
+    llarp::RouterContact rc;
+    const bool ret = rc.Read(file.c_str());
+
+    if(ret)
     {
-        llarp::RouterContact rc;
-        const bool ret = rc.Read(file.c_str());
-
-        if (ret)
-        {
-            if (json)
-            {
-                result[file] = rc.ToJson();
-            }
-            else
-            {
-            std::cout << "file = " << file << "\n";
-            std::cout << rc << "\n\n";
-            }
-        }
-        else
-        {
-            std::cerr << "file = " << file << " was not a valid rc file\n";
-        }
+      if(json)
+      {
+        result[file] = rc.ToJson();
+      }
+      else
+      {
+        std::cout << "file = " << file << "\n";
+        std::cout << rc << "\n\n";
+      }
     }
+    else
+    {
+      std::cerr << "file = " << file << " was not a valid rc file\n";
+    }
+  }
 
-    if (json)
-        std::cout << result << "\n";
+  if(json)
+    std::cout << result << "\n";
 
-    return true;
+  return true;
 }
 
 int
-main(int argc, char *argv[])
+main(int argc, char* argv[])
 {
 #ifdef LOKINET_DEBUG
   absl::SetMutexDeadlockDetectionMode(absl::OnDeadlockCycle::kAbort);
@@ -55,18 +56,20 @@ main(int argc, char *argv[])
       ("v,verbose", "Verbose", cxxopts::value<bool>())
       ("h,help", "help", cxxopts::value<bool>())
       ("j,json", "output in json", cxxopts::value<bool>())
-      ("dump", "dump rc file", cxxopts::value<std::vector<std::string>>(), "FILE");
+      ("dump", "dump rc file", cxxopts::value<std::vector<std::string> >(), "FILE");
   // clang-format on
 
-  try {
+  try
+  {
     const auto result = options.parse(argc, argv);
 
-    const bool json = result["json"].as<bool>();
+    const bool json = result["json"].as< bool >();
 
     if(result.count("verbose") > 0)
     {
       SetLogLevel(llarp::eLogDebug);
-      llarp::LogContext::Instance().logStream = std::make_unique<llarp::OStreamLogStream>(std::cerr);
+      llarp::LogContext::Instance().logStream =
+          std::make_unique< llarp::OStreamLogStream >(std::cerr);
       llarp::LogDebug("debug logging activated");
     }
 
@@ -78,13 +81,13 @@ main(int argc, char *argv[])
 
     if(result.count("dump") > 0)
     {
-      if (!dumpRc(result["dump"].as<std::vector<std::string>>(), json))
+      if(!dumpRc(result["dump"].as< std::vector< std::string > >(), json))
       {
-          return 1;
+        return 1;
       }
     }
   }
-  catch(const cxxopts::OptionParseException &ex)
+  catch(const cxxopts::OptionParseException& ex)
   {
     std::cerr << ex.what() << std::endl;
     std::cout << options.help() << std::endl;
