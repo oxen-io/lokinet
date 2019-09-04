4c77bf208fa92e6a478f4c42441bf29cf856568a
Jeff Becker <jeff@i2p.rocks>
2019-09-04 09:48:56 -0400

    update default netid

--- llarp/constants/version.cpp
+++ llarp/constants/version.cpp
@@ -1,6 +1,6 @@
 #include <constants/version.hpp>
 
 #ifndef LLARP_DEFAULT_NETID
-#define LLARP_DEFAULT_NETID "testnet"
+#define LLARP_DEFAULT_NETID "beta"
 #endif
 const char Version::LLARP_NET_ID[] = LLARP_DEFAULT_NETID;
