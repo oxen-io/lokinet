5863e3382513d18915b44aabf6ce3edeed88ded4
Jeff Becker <jeff@i2p.rocks>
2019-09-16 12:21:21 -0400

    move it arround

--- llarp/path/ihophandler.hpp
+++ llarp/path/ihophandler.hpp
@@ -43,32 +43,11 @@ namespace llarp
       // handle data in upstream direction
       bool
       HandleUpstream(const llarp_buffer_t& X, const TunnelNonce& Y,
-                     AbstractRouter*)
-      {
-        if(m_UpstreamQueue == nullptr)
-          m_UpstreamQueue = std::make_shared< TrafficQueue_t >();
-        m_UpstreamQueue->emplace_back();
-        auto& pkt = m_UpstreamQueue->back();
-        pkt.first.resize(X.sz);
-        std::copy_n(X.base, X.sz, pkt.first.begin());
-        pkt.second = Y;
-        return true;
-      }
-
+                     AbstractRouter*);
       // handle data in downstream direction
       bool
       HandleDownstream(const llarp_buffer_t& X, const TunnelNonce& Y,
-                       AbstractRouter*)
-      {
-        if(m_DownstreamQueue == nullptr)
-          m_DownstreamQueue = std::make_shared< TrafficQueue_t >();
-        m_DownstreamQueue->emplace_back();
-        auto& pkt = m_DownstreamQueue->back();
-        pkt.first.resize(X.sz);
-        std::copy_n(X.base, X.sz, pkt.first.begin());
-        pkt.second = Y;
-        return true;
-      }
+                       AbstractRouter*);
 
       /// return timestamp last remote activity happened at
       virtual llarp_time_t
