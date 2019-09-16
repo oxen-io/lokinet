ae3fc3a395e55f21212dc916aa842609339bb49a
Michael <michael@loki.network>
2019-09-16 20:40:31 +0100

    Add initial macOS app

---
+++ ui-macos/lokinet/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  lokinet
+//
+//  Copyright © 2019 Loki. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
+
---
+++ ui-macos/lokinet/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  lokinet
+//
+//  Copyright © 2019 Loki. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+}
+
