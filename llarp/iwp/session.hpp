146ed392528d4f95f01e326d35267b6b1b3fe526
Jeff Becker <jeff@i2p.rocks>
2019-11-07 08:51:43 -0500

    try explicit acks

--- llarp/iwp/session.hpp
+++ llarp/iwp/session.hpp
@@ -148,7 +148,7 @@ namespace llarp
       const llarp_time_t m_CreatedAt;
       const Addr m_RemoteAddr;
       /// set me to true to send multiacks otherwise we will do explict acks
-      const bool m_MACK = true;
+      const bool m_MACK = false;
       /// set me to true to send DROP messages
       const bool m_DROP = false;
 
