d591394ad213727435610471bed602d45a284810
Jeff Becker <jeff@i2p.rocks>
2019-11-28 15:55:55 -0500

    dont process empty queues

--- llarp/path/transit_hop.cpp
+++ llarp/path/transit_hop.cpp
@@ -221,6 +221,8 @@ namespace llarp
           break;
         msgs.emplace_back(maybe.value());
       } while(true);
+      if(msgs.empty())
+        return;
       HandleAllUpstream(std::move(msgs), r);
     }
 
@@ -240,6 +242,8 @@ namespace llarp
           break;
         msgs.emplace_back(maybe.value());
       } while(true);
+      if(msgs.empty())
+        return;
       HandleAllDownstream(std::move(msgs), r);
     }
 
