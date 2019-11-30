fd02662d9f34e4dc8a745b7b18bf685f49983f41
Jason Rhinelander <jason@imaginary.ca>
2019-11-30 02:05:19 -0400

    Bump default netid to gamma for 0.6

--- llarp/constants/version.cpp
+++ llarp/constants/version.cpp
@@ -1,6 +1,6 @@
 #include <constants/version.hpp>
 
 #ifndef LLARP_DEFAULT_NETID
-#define LLARP_DEFAULT_NETID "beta"
+#define LLARP_DEFAULT_NETID "gamma"
 #endif
 const char Version::LLARP_NET_ID[] = LLARP_DEFAULT_NETID;
