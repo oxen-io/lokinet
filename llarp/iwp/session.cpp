1a6a66108da5a147fcc601761b1b92baffc0b424
Jeff Becker <jeff@i2p.rocks>
2019-11-29 21:12:11 -0500

    make it compile

--- llarp/iwp/session.cpp
+++ llarp/iwp/session.cpp
@@ -688,28 +688,30 @@ namespace llarp
         }
       }
       {
-        auto itr = m_RXMsgs.find(rxid);
+        const auto now = m_Parent->Now();
+        auto itr       = m_RXMsgs.find(rxid);
         if(itr == m_RXMsgs.end())
         {
-          auto msg_itr = m_RXMsgs
-                             .emplace(rxid,
-                                      InboundMessage{rxid, sz, std::move(h),
-                                                     m_Parent->Now()})
-                             .first;
-          sz = std::min(data.size()
-                            - (CommandOverhead + sizeof(uint16_t)
-                               + sizeof(uint64_t) + PacketOverhead + 32),
-                        sz);
-          const llarp_buffer_t buf(data.data() + (data.size() - sz), sz);
-          msg_itr->second.HandleData(0, buf, now);
-          if(sz <= FragmentSize)
+          itr = m_RXMsgs
+                    .emplace(
+                        rxid,
+                        InboundMessage{rxid, sz, std::move(h), m_Parent->Now()})
+                    .first;
+          size_t _sz =
+              std::min(data.size()
+                           - (CommandOverhead + sizeof(uint16_t)
+                              + sizeof(uint64_t) + PacketOverhead + 32),
+                       size_t{sz});
+          const llarp_buffer_t buf(data.data() + (data.size() - _sz), _sz);
+          itr->second.HandleData(0, buf, now);
+          if(_sz <= FragmentSize)
           {
-            if(not msg_itr->second.Verify())
+            if(not itr->second.Verify())
             {
               LogError("bad short xmit hash from ", m_RemoteAddr);
               return;
             }
-            auto msg = std::move(msg_itr->second);
+            auto msg = std::move(itr->second);
             const llarp_buffer_t buf(msg.m_Data);
             m_Parent->HandleMessage(this, buf);
             m_ReplayFilter.emplace(rxid, m_Parent->Now());
