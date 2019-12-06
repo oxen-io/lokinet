8d2c22fc722928ae4224d0e98d3a121bc4bb6fa8
Jason Rhinelander <jason@imaginary.ca>
2019-12-06 18:19:08 -0400

    Replace cppbackport with ghc-filesystem
    From https://github.com/gulrak/filesystem which is more up-to-date and
    looks better maintained than cppbackport.

---
+++ vendor/ghc-filesystem/test/multi2.cpp
@@ -0,0 +1,48 @@
+//---------------------------------------------------------------------------------------
+//
+// Copyright (c) 2018, Steffen Schümann <s.schuemann@pobox.com>
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+//    may be used to endorse or promote products derived from this software without
+//    specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//---------------------------------------------------------------------------------------
+#include "catch.hpp"
+#include <ghc/filesystem.hpp>
+namespace fs = ghc::filesystem;
+
+// This test and the one in multi1.cpp doesn't actualy test relevant functionality,
+// it is just used to check that it is possible to include filesystem.h in multiple
+// source files.
+TEST_CASE("Multifile-test 2", "[multi]")
+{
+    CHECK("/usr/local/bin" == fs::path("/usr/local/bin").generic_string());
+    std::string str = "/usr/local/bin";
+    std::u16string u16str = u"/usr/local/bin";
+    std::u32string u32str = U"/usr/local/bin";
+    CHECK(str == fs::path(str.begin(), str.end()));
+    CHECK(str == fs::path(u16str.begin(), u16str.end()));
+    CHECK(str == fs::path(u32str.begin(), u32str.end()));
+}
