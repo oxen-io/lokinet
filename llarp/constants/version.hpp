4fdff7f2336777568e46d748d090cf213f1c1acc
Jeff Becker <jeff@i2p.rocks>
2019-12-11 17:06:44 -0500

    consolidate versions

--- llarp/constants/version.hpp
+++ llarp/constants/version.hpp
@@ -1,33 +1,35 @@
 #ifndef LLARP_VERSION_HPP
 #define LLARP_VERSION_HPP
 
-#if defined(_WIN32) && defined(RC_INVOKED)
-#define LLARP_VERSION 0, 6, 0, 0
-#else
-
 #ifndef LLARP_VERSION_MAJ
-#define LLARP_VERSION_MAJ "0"
+#define LLARP_VERSION_MAJ 0
 #endif
 
 #ifndef LLARP_VERSION_MIN
-#define LLARP_VERSION_MIN "6"
+#define LLARP_VERSION_MIN 6
 #endif
 
 #ifndef LLARP_VERSION_PATCH
-#define LLARP_VERSION_PATCH "0"
+#define LLARP_VERSION_PATCH 0
 #endif
 
 #ifndef LLARP_VERSION_NUM
 #ifdef GIT_REV
-#define LLARP_VERSION_NUM                                             \
-  "-" LLARP_VERSION_MAJ "." LLARP_VERSION_MIN "." LLARP_VERSION_PATCH \
+#define LLARP_VERSION_NUM                                    \
+  "-LLARP_VERSION_MAJ.LLARP_VERSION_MIN.LLARP_VERSION_PATCH" \
   "-" GIT_REV
 #else
 #define LLARP_VERSION_NUM \
-  "-" LLARP_VERSION_MAJ "." LLARP_VERSION_MIN "." LLARP_VERSION_PATCH
+  "-LLARP_VERSION_MAJ.LLARP_VERSION_MIN.LLARP_VERSION_PATCH"
 #endif
 #endif
+
+#if defined(_WIN32) && defined(RC_INVOKED)
+#define LLARP_VERSION \
+  LLARP_VERSION_MAJ, LLARP_VERSION_MIN, LLARP_VERSION_PATCH, 0
+#else
 #define LLARP_VERSION "lokinet" LLARP_VERSION_NUM
+#endif
 
 #ifndef LLARP_RELEASE_MOTTO
 #define LLARP_RELEASE_MOTTO "(dev build)"
@@ -37,5 +39,4 @@ struct Version
 {
   static const char LLARP_NET_ID[];
 };
-#endif
 #endif
