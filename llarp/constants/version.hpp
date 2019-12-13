68d8adcd8b35280f89042421147620a3095c78db
Jason Rhinelander <jason@imaginary.ca>
2019-12-12 21:32:00 -0400

    Offer ritual sacrifice to clang-format

--- llarp/constants/version.hpp
+++ llarp/constants/version.hpp
@@ -6,11 +6,11 @@
 namespace llarp
 {
   // Given a full lokinet version of: lokinet-1.2.3-abc these are:
-  extern const std::array<uint16_t, 3> VERSION; // [1, 2, 3]
-  extern const char* const VERSION_STR; // "1.2.3"
-  extern const char* const VERSION_TAG; // "abc"
-  extern const char* const VERSION_FULL; // "lokinet-1.2.3-abc"
+  extern const std::array< uint16_t, 3 > VERSION;  // [1, 2, 3]
+  extern const char* const VERSION_STR;            // "1.2.3"
+  extern const char* const VERSION_TAG;            // "abc"
+  extern const char* const VERSION_FULL;           // "lokinet-1.2.3-abc"
 
   extern const char* const RELEASE_MOTTO;
   extern const char* const DEFAULT_NETID;
-}
+}  // namespace llarp
