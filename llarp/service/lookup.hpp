96309a703e138819df6889b9ab72408df1c889eb
Jeff Becker <jeff@i2p.rocks>
2020-01-11 08:27:43 -0500

    tweak timeouts for path build

--- llarp/service/lookup.hpp
+++ llarp/service/lookup.hpp
@@ -36,7 +36,7 @@ namespace llarp
 
       /// determine if this request has timed out
       bool
-      IsTimedOut(llarp_time_t now, llarp_time_t timeout = 20000) const
+      IsTimedOut(llarp_time_t now, llarp_time_t timeout = 30000) const
       {
         if(now <= m_created)
           return false;
