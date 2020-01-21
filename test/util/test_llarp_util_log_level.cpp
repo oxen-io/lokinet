28752f85b7210036bc8a4e3baee3e1d37eb07da8
Jeff Becker <jeff@i2p.rocks>
2020-01-21 13:35:52 -0500

    improve test cases

--- test/util/test_llarp_util_log_level.cpp
+++ test/util/test_llarp_util_log_level.cpp
@@ -1,12 +1,41 @@
 #include <gtest/gtest.h>
 #include <util/logging/loglevel.hpp>
 
-struct LogLevelTest : public ::testing::Test
+using TestString = std::string;
+
+struct TestParseLog
+{
+  TestString input;
+  absl::optional< llarp::LogLevel > level;
+};
+
+struct LogLevelTest : public ::testing::TestWithParam< TestParseLog >
 {
 };
 
-TEST_F(LogLevelTest, TestLogLevelNameBadName)
+TEST_P(LogLevelTest, parseLevel)
 {
-  const auto maybe = llarp::LogLevelFromString("bogus");
-  ASSERT_FALSE(maybe.has_value());
+  const auto data  = GetParam();
+  const auto maybe = llarp::LogLevelFromString(data.input);
+  EXPECT_EQ(maybe, data.level);
 }
+
+static const TestParseLog testParseLog[] = {
+    // bad cases
+    {"bogus", {}},
+    {"BOGUS", {}},
+    {"", {}},
+    {" ", {}},
+    // good cases
+    {"info", llarp::eLogInfo},
+    {"infO", llarp::eLogInfo},
+    {"iNfO", llarp::eLogInfo},
+    {"InfO", llarp::eLogInfo},
+    {"INFO", llarp::eLogInfo},
+    {"debug", llarp::eLogDebug},
+    {"warn", llarp::eLogWarn},
+    {"error", llarp::eLogError},
+    {"none", llarp::eLogNone}};
+
+INSTANTIATE_TEST_CASE_P(TestLogConfig, LogLevelTest,
+                        ::testing::ValuesIn(testParseLog), );
