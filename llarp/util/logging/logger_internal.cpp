368acec485c77438a45bbb66ee60934eb9fcb7e7
Jeff Becker <jeff@i2p.rocks>
2020-02-25 12:19:40 -0500

    fix last commit

--- llarp/util/logging/logger_internal.cpp
+++ llarp/util/logging/logger_internal.cpp
@@ -7,17 +7,9 @@ namespace llarp
   std::ostream&
   operator<<(std::ostream& out, const log_timestamp& ts)
   {
-    std::chrono::milliseconds delta{ts.delta};
-    auto h = std::chrono::duration_cast< std::chrono::hours >(delta);
-    delta -= h;
-    auto m = std::chrono::duration_cast< std::chrono::minutes >(delta);
-    delta -= m;
-    auto s = std::chrono::duration_cast< std::chrono::seconds >(delta);
-    delta -= s;
-    llarp_time_t ms = delta;
     std::chrono::time_point< std::chrono::system_clock,
                              std::chrono::milliseconds >
         now{std::chrono::milliseconds{ts.now}};
-    return date::operator<<(out, now) << " UTC [+" << ms << "]";
+    return date::operator<<(out, now) << " UTC [+" << ts.delta << "]";
   }
 }  // namespace llarp
