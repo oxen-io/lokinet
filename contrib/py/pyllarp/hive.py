5dcf2713396c18c6b17a9e80d19e91ad172b2501
Jeff Becker <jeff@i2p.rocks>
2020-02-27 18:03:33 -0500

    update python

--- contrib/py/pyllarp/hive.py
+++ contrib/py/pyllarp/hive.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 import pyllarp
 
-def main():
+def main(conf):
   hive = pyllarp.RouterHive()
   config = pyllarp.Config()
+  print("loading config: {}".format(conf))
+  if not config.LoadFile(conf):
+    print("failed to load {}".format(conf))
+    return
   hive.AddRouter(config)
   hive.StartAll()
   hive.StopAll()
 
 if __name__ == '__main__':
-  main()
+  import sys
+  main(sys.argv[1])
