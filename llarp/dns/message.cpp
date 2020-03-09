2fc1d3ae5bf859eaad7dd976fed9d86e48bd668c
Jeff Becker <jeff@i2p.rocks>
2020-03-09 13:01:21 -0400

    fix dns replies so that extra RR for edns are not included.

--- llarp/dns/message.cpp
+++ llarp/dns/message.cpp
@@ -79,13 +79,14 @@ namespace llarp
     bool
     Message::Encode(llarp_buffer_t* buf) const
     {
+      LogInfo("Encode ", *this);
       MessageHeader hdr;
       hdr.id       = hdr_id;
       hdr.fields   = hdr_fields;
       hdr.qd_count = questions.size();
       hdr.an_count = answers.size();
-      hdr.ns_count = authorities.size();
-      hdr.ar_count = additional.size();
+      hdr.ns_count = 0;
+      hdr.ar_count = 0;
 
       if(!hdr.Encode(buf))
         return false;
@@ -96,14 +97,6 @@ namespace llarp
 
       for(const auto& answer : answers)
         if(!answer.Encode(buf))
-          return false;
-
-      for(const auto& auth : authorities)
-        if(!auth.Encode(buf))
-          return false;
-
-      for(const auto& rr : additional)
-        if(!rr.Encode(buf))
           return false;
 
       return true;
@@ -162,12 +155,18 @@ namespace llarp
       }
     }
 
+    static constexpr uint16_t
+    reply_flags(uint16_t setbits)
+    {
+      return setbits | flags_QR | flags_AA | flags_RA;
+    }
+
     void
     Message::AddINReply(llarp::huint128_t ip, bool isV6, RR_TTL_t ttl)
     {
       if(questions.size())
       {
-        hdr_fields |= flags_QR | flags_AA | flags_RA;
+        hdr_fields = reply_flags(hdr_fields);
         ResourceRecord rec;
         rec.rr_name  = questions[0].qname;
         rec.rr_class = qClassIN;
@@ -193,7 +192,8 @@ namespace llarp
     {
       if(questions.size())
       {
-        hdr_fields |= flags_QR | flags_AA | flags_RA;
+        hdr_fields = reply_flags(hdr_fields);
+
         const auto& question = questions[0];
         answers.emplace_back();
         auto& rec                     = answers.back();
@@ -217,7 +217,8 @@ namespace llarp
     {
       if(questions.size())
       {
-        hdr_fields |= flags_QR | flags_AA | flags_RA;
+        hdr_fields = reply_flags(hdr_fields);
+
         const auto& question = questions[0];
         answers.emplace_back();
         auto& rec                     = answers.back();
@@ -241,7 +242,8 @@ namespace llarp
     {
       if(questions.size())
       {
-        hdr_fields |= flags_QR | flags_AA;
+        hdr_fields = reply_flags(hdr_fields);
+
         const auto& question = questions[0];
         answers.emplace_back();
         auto& rec                     = answers.back();
@@ -266,7 +268,7 @@ namespace llarp
       if(questions.size())
       {
         // authorative response with recursion available
-        hdr_fields |= flags_QR | flags_AA | flags_RA;
+        hdr_fields = reply_flags(hdr_fields);
         // don't allow recursion on this request
         hdr_fields &= ~flags_RD;
         hdr_fields |= flags_RCODENameError;
--- llarp/handlers/tun.cpp
+++ llarp/handlers/tun.cpp
@@ -554,7 +554,7 @@ namespace llarp
           }
           else
           {
-            auto replyMsg = std::make_shared< dns::Message >(std::move(msg));
+            auto replyMsg = std::make_shared< dns::Message >(msg);
             using service::Address;
             using service::OutboundContext;
             return EnsurePathToService(
