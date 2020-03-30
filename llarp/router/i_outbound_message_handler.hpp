ce939e7ed7f0cad88fc15a90533cf0a92102b44d
Jeff Becker <jeff@i2p.rocks>
2020-03-30 07:28:56 -0400

    outbound messagee handler docs

--- llarp/router/i_outbound_message_handler.hpp
+++ llarp/router/i_outbound_message_handler.hpp
@@ -8,13 +8,22 @@
 
 namespace llarp
 {
+  /**
+     The result of sending an outbound message to a peer
+   */
   enum class SendStatus
   {
+    /** the operation was successful */
     Success,
+    /** the operation timed out */
     Timeout,
+    /** there was no appropriate link to deliver the message */
     NoLink,
+    /** the router to deliver to is deemed invalid by network policy */
     InvalidRouter,
+    /** the router to deliver to is not known and could not be looked up */
     RouterNotFound,
+    /** network congestion prevented delivery */
     Congestion
   };
 
@@ -22,26 +31,52 @@ namespace llarp
   struct RouterID;
   struct PathID_t;
 
+  /** hook function for propagating SendStatus result to caller */
   using SendStatusHandler = std::function< void(SendStatus) >;
 
-  static const size_t MAX_PATH_QUEUE_SIZE            = 40;
-  static const size_t MAX_OUTBOUND_QUEUE_SIZE        = 200;
+  /** maximum queue size for paths in number of messages */
+  static const size_t MAX_PATH_QUEUE_SIZE = 40;
+  /** maximum number of queues to flush per tick */
+  static const size_t MAX_OUTBOUND_QUEUE_SIZE = 200;
+  /** maxium number of messages to send per queue */
   static const size_t MAX_OUTBOUND_MESSAGES_PER_TICK = 20;
 
+  /**
+     Outbound Message Delivery Interface
+     @brief handles queuing and delivery of outbound messages
+   */
   struct IOutboundMessageHandler
   {
     virtual ~IOutboundMessageHandler() = default;
 
+    /**
+       @brief serialize and queue a message for delivery
+
+       @param remote router id of router to deliver to
+       @param msg a pointer to a LinkMessage to serialize
+       @param callback hook to handle delivery
+       @returns true if we successfully serialized and queued the message, false
+       on serialize fail.
+     */
     virtual bool
     QueueMessage(const RouterID &remote, const ILinkMessage *msg,
                  SendStatusHandler callback) = 0;
 
+    /**
+       @brief process queues
+     */
     virtual void
     Tick() = 0;
 
+    /**
+       @brief remove path by path id from being processed
+     */
     virtual void
     QueueRemoveEmptyPath(const PathID_t &pathid) = 0;
 
+    /**
+       @brief introspect for RPC
+     */
     virtual util::StatusObject
     ExtractStatus() const = 0;
   };
