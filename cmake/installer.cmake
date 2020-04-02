53bef479a37be003ee581f8a84203928fc70bbff
Jeff Becker <jeff@i2p.rocks>
2020-04-02 11:53:14 -0400

    more macos installer bits

--- cmake/installer.cmake
+++ cmake/installer.cmake
@@ -4,12 +4,17 @@ set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "lokinet - onion router thingydoo")
 set(CPACK_PACKAGE_VERSION_MAJOR "${LLARP_VERSION_MAJOR}")
 set(CPACK_PACKAGE_VERSION_MINOR "${LLARP_VERSION_MINOR}")
 set(CPACK_PACKAGE_VERSION_PATCH "${LLARP_VERSION_PATCH}")
-set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/LICENSE")
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/LICENSE.txt")
 set(CPACK_PACKAGE_INSTALL_DIRECTORY "lokinet")
 
 if(WIN32)
   include(cmake/win32_installer_deps.cmake)
 endif()
+
+if(APPLE)
+  include(cmake/macos_installer_deps.cmake)
+endif()
+  
 
 # This must always be last!
 include(CPack)
---
+++ cmake/macos_installer_deps.cmake
@@ -0,0 +1,2 @@
+# macos specific cpack stuff goes here
+set(CPACK_PRODUCTBUILD_RESOURCES_DIR ${CMAKE_SOURCE_DIR}/osx-setup/resources)
