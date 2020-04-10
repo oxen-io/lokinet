b630a3135a460ebb9ae44168b656fbbe09d09971
Jeff Becker <jeff@i2p.rocks>
2020-04-10 15:17:11 -0400

    macos post install script that bootstraps, generates configs and registers plist

--- cmake/macos_installer_deps.cmake
+++ cmake/macos_installer_deps.cmake
@@ -1,3 +1,4 @@
 # macos specific cpack stuff goes here
 set(CPACK_GENERATOR "productbuild")
 set(CPACK_PACKAGING_INSTALL_PREFIX "/usr/local")
+set(CPACK_POSTFLIGHT_LOKINET_SCRIPT ${CMAKE_SOURCE_DIR}/contrib/macos/postinstall)
