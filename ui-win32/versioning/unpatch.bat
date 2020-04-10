88ae4bdd776c5e28ddac11899b33ba31cb13f0d4
Jeff Becker <jeff@i2p.rocks>
2020-04-10 15:31:15 -0400

    gut ui-*

--- ui-win32/versioning/unpatch.bat
+++
@@ -1,4 +0,0 @@
-REM this requires some kind of cygwin toolset in $PATH
-REM or just having sed(1) at a minimum
-set /P RELEASE_NAME=<..\..\..\motto.txt
-sed -i "s/%RELEASE_NAME%/RELEASE_CODENAME/g" ..\..\Properties\AssemblyInfo.cs
