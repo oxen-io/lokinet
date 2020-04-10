c2fbc28bbfecfce57786e55f5415a51ce543a29e
Jeff Becker <jeff@i2p.rocks>
2020-04-10 15:31:15 -0400

    gut ui-*

--- ui-win32/versioning/release.bat
+++
@@ -1,4 +0,0 @@
-REM this requires some kind of cygwin toolset in $PATH
-REM or just having sed(1) at a minimum
-set /P RELEASE_NAME=<..\..\..\motto.txt
-sed -i "s/RELEASE_CODENAME/%RELEASE_NAME%/g" ..\..\Properties\AssemblyInfo.cs
