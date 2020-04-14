dd3121d8acb9aabb6fa4d9b3d7ab3b58aa2ee8c7
Stephen Shelton <steve@brewcraft.org>
2020-04-14 15:48:33 -0600

    MacOS installer / runtime nonsense

--- cmake/macos_installer_deps.cmake
+++ cmake/macos_installer_deps.cmake
@@ -1,4 +1,10 @@
 # macos specific cpack stuff goes here
+
+# copy files that will be later moved by the postinstall script to proper locations
+install(FILES ${CMAKE_SOURCE_DIR}/contrib/macos/lokinet_macos_daemon_script.sh
+              ${CMAKE_SOURCE_DIR}/contrib/macos/network.loki.lokinet.daemon.plist
+	    DESTINATION "extra/")
+
 set(CPACK_GENERATOR "productbuild")
 set(CPACK_PACKAGING_INSTALL_PREFIX "/usr/local")
 set(CPACK_POSTFLIGHT_LOKINET_SCRIPT ${CMAKE_SOURCE_DIR}/contrib/macos/postinstall)
