a1aba58dfe07330ee3cf3c67997c689ea6c2d9c6
Jeff Becker <jeff@i2p.rocks>
2020-05-12 12:24:08 -0400

    queue size increase

--- llarp/router/i_outbound_message_handler.hpp
+++ llarp/router/i_outbound_message_handler.hpp
@@ -24,9 +24,9 @@ namespace llarp
 
   using SendStatusHandler = std::function<void(SendStatus)>;
 
-  static const size_t MAX_PATH_QUEUE_SIZE = 50;
-  static const size_t MAX_OUTBOUND_QUEUE_SIZE = 200;
-  static const size_t MAX_OUTBOUND_MESSAGES_PER_TICK = 100;
+  static const size_t MAX_PATH_QUEUE_SIZE = 100;
+  static const size_t MAX_OUTBOUND_QUEUE_SIZE = 1000;
+  static const size_t MAX_OUTBOUND_MESSAGES_PER_TICK = 500;
 
   struct IOutboundMessageHandler
   {
