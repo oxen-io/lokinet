bbf2020bf86e99e2527bf1bdb80bb96814fa0c70
Stephen Shelton <steve@brewcraft.org>
2020-06-15 11:34:40 -0600

    Add missing files

---
+++ test/hive/test_peer_stats.py
@@ -0,0 +1,44 @@
+import pyllarp
+from time import time
+
+def test_peer_stats(HiveForPeerStats):
+  hive = HiveForPeerStats(n_relays=5, n_clients=0, netid="hive")
+
+  start_time = time()
+  cur_time = start_time
+  test_duration = 30 #seconds
+
+  paths = []
+
+  print("looking for events...")
+
+  numInbound = 0
+  numOutbound = 0
+  numAttempts = 0
+
+  while cur_time < start_time + test_duration:
+
+    hive.CollectAllEvents()
+
+    for event in hive.events:
+      event_name = event.__class__.__name__
+
+      if event_name == "LinkSessionEstablishedEvent":
+        if event.inbound:
+          numInbound += 1
+        else:
+          numOutbound += 1
+
+      if event_name == "ConnectionAttemptEvent":
+        numAttempts += 1
+
+    hive.events = []
+    cur_time = time()
+
+  print("test duration exceeded")
+  print("in: {} out: {} attempts: {}", numInbound, numOutbound, numAttempts);
+  assert(numInbound == numOutbound)
+  assert(numOutbound == numAttempts)
+
+if __name__ == "__main__":
+  main()
