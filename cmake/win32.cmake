c2f9c466867aed30201a7cd61ec95fd7d7a24258
Jeff Becker <jeff@i2p.rocks>
2020-06-24 10:17:43 -0400

    zmq requires win xp or higher.

--- cmake/win32.cmake
+++ cmake/win32.cmake
@@ -14,7 +14,8 @@ if(NOT MSVC_VERSION)
   # to .r[o]data section one after the other!
   add_compile_options(-fno-ident -Wa,-mbig-obj)
   link_libraries( -lws2_32 -lshlwapi -ldbghelp -luser32 -liphlpapi -lpsapi -luserenv )
-  add_definitions(-DWINVER=0x0500 -D_WIN32_WINNT=0x0500)
+  # zmq requires windows xp or higher
+  add_definitions(-DWINVER=0x0501 -D_WIN32_WINNT=0x0501)
 endif()
 
 if(EMBEDDED_CFG)
