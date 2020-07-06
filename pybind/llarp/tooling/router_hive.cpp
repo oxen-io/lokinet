56e2bc2c4774ec6e6f3213285db80d7afa49cd26
Stephen Shelton <steve@brewcraft.org>
2020-07-06 17:40:42 -0600

    Acquire python's Global Interpreter Lock in callbacks

--- pybind/llarp/tooling/router_hive.cpp
+++ pybind/llarp/tooling/router_hive.cpp
@@ -12,6 +12,9 @@ namespace tooling
   RouterHive_Init(py::module& mod)
   {
     using RouterHive_ptr = std::shared_ptr<RouterHive>;
+    using Context_ptr = RouterHive::Context_ptr;
+    using ContextVisitor = std::function<void(Context_ptr)>;
+
     py::class_<RouterHive, RouterHive_ptr>(mod, "RouterHive")
         .def(py::init<>())
         .def("AddRelay", &RouterHive::AddRelay)
@@ -19,9 +22,30 @@ namespace tooling
         .def("StartRelays", &RouterHive::StartRelays)
         .def("StartClients", &RouterHive::StartClients)
         .def("StopAll", &RouterHive::StopRouters)
-        .def("ForEachRelay", &RouterHive::ForEachRelay)
-        .def("ForEachClient", &RouterHive::ForEachClient)
-        .def("ForEachRouter", &RouterHive::ForEachRouter)
+        .def(
+            "ForEachRelay",
+            [](RouterHive& hive, ContextVisitor visit) {
+              hive.ForEachRelay([visit](Context_ptr ctx) {
+                py::gil_scoped_acquire acquire;
+                visit(std::move(ctx));
+              });
+            })
+        .def(
+            "ForEachClient",
+            [](RouterHive& hive, ContextVisitor visit) {
+              hive.ForEachClient([visit](Context_ptr ctx) {
+                py::gil_scoped_acquire acquire;
+                visit(std::move(ctx));
+              });
+            })
+        .def(
+            "ForEachRouter",
+            [](RouterHive& hive, ContextVisitor visit) {
+              hive.ForEachRouter([visit](Context_ptr ctx) {
+                py::gil_scoped_acquire acquire;
+                visit(std::move(ctx));
+              });
+            })
         .def("GetNextEvent", &RouterHive::GetNextEvent)
         .def("GetAllEvents", &RouterHive::GetAllEvents)
         .def("RelayConnectedRelays", &RouterHive::RelayConnectedRelays)
