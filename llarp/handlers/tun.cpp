98e022ea218e87973ee2ccec7d6f1d7d0ce6ea0e
Jeff <jeff@i2p.rocks>
2020-10-12 13:55:58 -0400

    Unflaky-ify lns dns (#1406)
    * always ensure path to service on dns lookup
    
    * deprecate profiles option

--- llarp/handlers/tun.cpp
+++ llarp/handlers/tun.cpp
@@ -311,13 +311,6 @@ namespace llarp
                                          service::Address addr, auto msg, bool isV6) -> bool {
         using service::Address;
         using service::OutboundContext;
-        if (self->HasAddress(addr))
-        {
-          const auto ip = self->ObtainIPForAddr(addr, false);
-          msg->AddINReply(ip, isV6);
-          reply(*msg);
-          return true;
-        }
         return self->EnsurePathToService(
             addr,
             [=](const Address&, OutboundContext* ctx) {
