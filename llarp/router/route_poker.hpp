d8cfcf4aeb2cebafe701264684e83df26f4ff40a
Thomas Winget <tewinget@gmail.com>
2020-10-15 22:50:46 -0400

    Route Poker changes
    If not using an exit, messing with the routing table is not good.
    As such, the ability to keep track of routes we *might* want is good,
    but the ability to set/unset those routes is necessary, to correspond
    to enabling/disabling exit functionality.

--- llarp/router/route_poker.hpp
+++ llarp/router/route_poker.hpp
@@ -16,16 +16,49 @@ namespace llarp
     void
     DelRoute(huint32_t ip);
 
+    void
+    Init(AbstractRouter* router, bool enable = false);
+
     ~RoutePoker();
 
     void
-    Update(const AbstractRouter& router);
+    Update();
+
+    // sets stored routes and causes AddRoute to actually
+    // set routes rather than just storing them
+    void
+    Enable();
+
+    // unsets stored routes, and causes AddRoute to simply
+    // remember the desired routes rather than setting them.
+    void
+    Disable();
 
    private:
+    void
+    DeleteAllRoutes();
+
+    void
+    DisableAllRoutes();
+
+    void
+    EnableAllRoutes();
+
+    void
+    EnableRoute(huint32_t ip, huint32_t gateway);
+
+    void
+    DisableRoute(huint32_t ip, huint32_t gateway);
+
     std::optional<huint32_t>
-    GetDefaultGateway(const AbstractRouter& router) const;
+    GetDefaultGateway() const;
 
     std::unordered_map<huint32_t, huint32_t> m_PokedRoutes;
     huint32_t m_CurrentGateway;
+
+    bool m_Enabled = false;
+    bool m_Enabling = false;
+
+    AbstractRouter* m_Router = nullptr;
   };
 }  // namespace llarp
