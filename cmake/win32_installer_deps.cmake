ad74d7da3c58dbdc2dfb75c5a441c406150964e2
Jeff Becker <jeff@i2p.rocks>
2020-10-22 17:14:44 -0400

    redo cpack for win32
    * bundle gui
    * bundle bootstrap.signed
    * add shortcut
    * install/remove service

--- cmake/win32_installer_deps.cmake
+++ cmake/win32_installer_deps.cmake
@@ -1,9 +1,36 @@
+
+
+set(GUI_ZIP_URL "https://builds.lokinet.dev/loki-project/loki-network-control-panel/master/lokinet-gui-windows-32bit-20201019T124842Z-136de83e1.zip")
 set(TUNTAP_URL "https://build.openvpn.net/downloads/releases/latest/tap-windows-latest-stable.exe")
 set(TUNTAP_EXE "${CMAKE_BINARY_DIR}/tuntap-install.exe")
+set(BOOTSTRAP_URL "https://seed.lokinet.org/lokinet.signed")
+set(BOOTSTRAP_FILE "${CMAKE_BINARY_DIR}/bootstrap.signed")
+
 file(DOWNLOAD
     ${TUNTAP_URL}
     ${TUNTAP_EXE})
+
+file(DOWNLOAD
+    ${BOOTSTRAP_URL}
+    ${BOOTSTRAP_FILE})
+
+file(DOWNLOAD
+    ${GUI_ZIP_URL}
+    ${CMAKE_BINARY_DIR}/lokinet-gui.zip)
+
+execute_process(COMMAND ${CMAKE_COMMAND} -E tar xf ${CMAKE_BINARY_DIR}/lokinet-gui.zip
+  WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+
+install(DIRECTORY ${CMAKE_BINARY_DIR}/gui DESTINATION share COMPONENT gui)
 install(PROGRAMS ${TUNTAP_EXE} DESTINATION bin)
+install(FILES ${BOOTSTRAP_FILE} DESTINATION share)
+
 set(CPACK_NSIS_DEFINES "RequestExecutionLevel admin")
-set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS "ExecWait '$INSTDIR\\\\bin\\\\tuntap-install.exe /S'")
-set(CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS "ExecWait 'C:\\\\Program Files\\\\TAP-Windows\\\\Uninstall.exe /S'")
+set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS "ExecWait '$INSTDIR\\\\bin\\\\tuntap-install.exe /S'\\nExecWait '$INSTDIR\\\\bin\\\\lokinet.exe --install'\\nExecWait '$INSTDIR\\\\bin\\\\lokinet.exe -g C:\\\\ProgramData\\\\lokinet\\\\lokinet.ini'\\nCopyFiles '$INSTDIR\\\\share\\\\bootstrap.signed' C:\\\\ProgramData\\\\lokinet\\\\bootstrap.signed")
+set(CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS "ExecWait '$INSTDIR\\\\bin\\\\bin\\\\lokinet.exe --remove'\\nRMDir /r /REBOOTOK C:\\\\ProgramData\\\\lokinet")
+set(CPACK_NSIS_CREATE_ICONS_EXTRA
+    "CreateShortCut '$SMPROGRAMS\\\\$STARTMENU_FOLDER\\\\Lokinet.lnk' '$INSTDIR\\\\share\\\\gui\\\\lokinet-gui.exe'"
+)
+set(CPACK_NSIS_DELETE_ICONS_EXTRA
+    "Delete '$SMPROGRAMS\\\\$START_MENU\\\\Lokinet.lnk'"
+)
