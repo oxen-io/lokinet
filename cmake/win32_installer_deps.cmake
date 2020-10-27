ae4af464379ec543709584a64941049ab5fba3e0
Jeff Becker <jeff@i2p.rocks>
2020-10-27 12:00:48 -0400

    bump win32 gui build\  * add hashpin for gui  * add installer icon

--- cmake/win32_installer_deps.cmake
+++ cmake/win32_installer_deps.cmake
@@ -1,6 +1,5 @@
-
-
-set(GUI_ZIP_URL "https://builds.lokinet.dev/loki-project/loki-network-control-panel/master/lokinet-gui-windows-32bit-20201019T124842Z-136de83e1.zip")
+set(GUI_ZIP_URL "https://builds.lokinet.dev/loki-project/loki-network-control-panel/master/lokinet-gui-windows-32bit-20201027T145055Z-c401b3279.zip")
+set(GUI_ZIP_HASH SHA256=72259bf7d515a2a5dbc490a6c5c79acd2d64d748ecb8b0be9b45a63e0f46440b)
 set(TUNTAP_URL "https://build.openvpn.net/downloads/releases/latest/tap-windows-latest-stable.exe")
 set(TUNTAP_EXE "${CMAKE_BINARY_DIR}/tuntap-install.exe")
 set(BOOTSTRAP_URL "https://seed.lokinet.org/lokinet.signed")
@@ -16,7 +15,8 @@ file(DOWNLOAD
 
 file(DOWNLOAD
     ${GUI_ZIP_URL}
-    ${CMAKE_BINARY_DIR}/lokinet-gui.zip)
+    ${CMAKE_BINARY_DIR}/lokinet-gui.zip
+    EXPECTED_HASH ${GUI_ZIP_HASH})
 
 execute_process(COMMAND ${CMAKE_COMMAND} -E tar xf ${CMAKE_BINARY_DIR}/lokinet-gui.zip
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
@@ -25,6 +25,7 @@ install(DIRECTORY ${CMAKE_BINARY_DIR}/gui DESTINATION share COMPONENT gui)
 install(PROGRAMS ${TUNTAP_EXE} DESTINATION bin)
 install(FILES ${BOOTSTRAP_FILE} DESTINATION share)
 
+set(CPACK_NSIS_MUI_ICON "${CMAKE_SOURCE_DIR}/win32-setup/lokinet.ico")
 set(CPACK_NSIS_DEFINES "RequestExecutionLevel admin")
 set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS "ExecWait '$INSTDIR\\\\bin\\\\tuntap-install.exe /S'\\nExecWait '$INSTDIR\\\\bin\\\\lokinet.exe --install'\\nExecWait '$INSTDIR\\\\bin\\\\lokinet.exe -g C:\\\\ProgramData\\\\lokinet\\\\lokinet.ini'\\nCopyFiles '$INSTDIR\\\\share\\\\bootstrap.signed' C:\\\\ProgramData\\\\lokinet\\\\bootstrap.signed")
 set(CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS "ExecWait '$INSTDIR\\\\bin\\\\bin\\\\lokinet.exe --remove'\\nRMDir /r /REBOOTOK C:\\\\ProgramData\\\\lokinet")
