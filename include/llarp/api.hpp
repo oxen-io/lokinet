1a7ea95ded4e920d6621039a4d00388a28533b21
Jeff Becker <jeff@i2p.rocks>
2021-01-25 13:49:43 -0500

    more api stuff

--- include/llarp/api.hpp
+++ include/llarp/api.hpp
@@ -8,15 +8,22 @@ namespace llarp::api
 {
   class Endpoint;
 
-  class NetAddressImpl;
-
   // an address we can send and recv packets to and from
+  // copyable but non moveable
   class NetAddress
   {
     NetAddressImpl* const m_Impl;
 
    public:
-    explicit NetAddress(NetAddressImpl* impl);
+    explicit NetAddress(NetAddressImpl*);
+
+    /// copy constructor
+    NetAddress(const NetAddress&);
+
+    /// copy assignment
+    NetAddress&
+    operator=(const NetAddress&);
+
     ~NetAddress();
 
     /// get string representation of this address
@@ -28,10 +35,43 @@ namespace llarp::api
     {
       return out << ToString();
     }
+
+    bool
+    Equals(const NetAddress&) const;
+
+    /// equality operator
+    bool
+    operator==(const NetAddress& other) const
+    {
+      return Equals(other);
+    }
+
+    /// inequality operator
+    bool
+    operator!=(const NetAddress& other) const
+    {
+      return not Equals(other);
+    }
+  };
+
+  /// a type in charge of handling events on a stream
+  class IStreamHandler
+  {
+   public:
+    virtual ~IStreamHandler() = default;
+
+    /// handles when we read data from the remote peer
+    virtual void OnRecv(std::string_view) = 0;
+
+    /// handles when we have established the stream with the remote peer
+    /// does not fire for inbound connections
+    virtual void
+    OnConnected() = 0;
   };
 
   class StreamImpl;
 
+  /// a stream oriented connection that receives and sends data in order
   class Stream
   {
     StreamImpl* const m_Impl;
@@ -40,30 +80,33 @@ namespace llarp::api
     explicit Stream(StreamImpl* impl);
     ~Stream();
 
+    /// attach a handler to this stream
+    void
+    AttachHandler(std::unique_ptr<IStreamHandler> handler);
+
+    /// get remote address we are connected to
+    /// throws if not connected
     NetAddress
     RemoteAddress() const;
 
+    /// get remote port we are talking to
+    /// throws if not connected
     uint16_t
     RemotePort() const;
 
+    /// get local address we are on
     NetAddress
     LocalAddress() const;
 
+    /// get local port we are on
     uint16_t
     LocalPort() const;
-
-    /// handle ordered inbound data from remote endpoint
-    virtual void OnRecv(std::string_view){};
 
     /// send data to remote endpoint in an ordered fashion
     /// return true if the data was queued, returns false if the data was not queued and the
     /// operation would have blocked
     bool
     Send(std::string_view data);
-
-    /// make an outbound connection to a remote endpoint
-    void
-    Connect(NetAddress remoteAddr, uint16_t remotePort);
 
     /// close the connection
     void
@@ -72,10 +115,6 @@ namespace llarp::api
     /// return true if this connection is closed
     bool
     IsClosed() const;
-
-    /// called when the connection is established
-    virtual void
-    OnConnected(){};
   };
 
   class StreamAcceptorImpl;
@@ -85,12 +124,56 @@ namespace llarp::api
   {
     StreamAcceptorImpl* const m_Impl;
 
+   protected:
+    /// create a handler to put into the stream when we get an inbound stream
+    virtual std::unique_ptr<IStreamHandler>
+    CreateStreamHandler() = 0;
+
    public:
     explicit StreamAcceptor(std::shared_ptr<Endpoint> ep, uint16_t localPort);
     ~StreamAcceptor();
 
-    /// called when we accepted a new inbound stream from the void
-    virtual void AcceptNewStream(std::shared_ptr<Stream>){};
+    /// start accepting connections
+    void
+    Listen();
+
+    /// stop accepting connections and close all open connections
+    void
+    Close();
+
+    /// handle when we get a new inbound connection
+    virtual void
+    HandleInboundConnection(std::shared_ptr<Stream> inbound) = 0;
+  };
+
+  class StreamConnectorImpl;
+
+  class StreamConnector
+  {
+    StreamConnectorImpl* const m_Impl;
+
+   public:
+    /// construct a stream connector to try to connect from endpoint to remoteAddr on remotePort
+    /// durring construction packets are sent out to the network
+    /// calls OnConnected / OnConnectFailure when this connection attempt concludes after
+    /// which this StreamConnector is safe to destruct
+    explicit StreamConnector(
+        std::shared_ptr<Endpoint> ep, NetAddress remoteAddr, uint16_t remotePort);
+    ~StreamConnector();
+
+    /// connection result type
+    enum class FailType
+    {
+      eTimeout,
+      eRefused,
+      eReset
+    };
+
+    /// handle connection failure passes in why the connection failed
+    virtual void OnConnectFailure(FailType) = 0;
+
+    /// handle connection success
+    virtual void OnConnected(std::shared<Stream>) = 0;
   };
 
   class UDPSocketImpl;
@@ -112,7 +195,7 @@ namespace llarp::api
     /// handle an inbound udp packet from the void
     /// implement in subtype
     virtual void
-    HandlePacketFrom(NetAddress fromAddr, uint16_t fromPort, std::string_view data){};
+    HandlePacketFrom(NetAddress fromAddr, uint16_t fromPort, std::string_view data) = 0;
 
     /// close the socket forever
     void
@@ -121,6 +204,7 @@ namespace llarp::api
 
   class EndpointPrivKeysImpl;
 
+  /// persistent private key material
   class EndpointPrivKeys
   {
     /// private implementation
@@ -137,17 +221,15 @@ namespace llarp::api
   class Endpoint
   {
    public:
+    /// constructs an endpoint with optionally provided endpoint keys
+    /// if endpoint keys are not provided then ephemeral keys will be generated and used
     explicit Endpoint(std::unique_ptr<EndpointPrivKeys> keys = nullptr);
     ~Endpoint();
 
     // intrnal implementation
     EndpointImpl* const Impl;
 
-    /// make an outbound stream ready to be connected to a remote endpoint
-    std::shared_ptr<Stream>
-    OutboundStream();
-
-    /// obtain our network address
+    /// obtain our own network address
     NetAddress
     GetOurAddress();
 
