046ab3d4535bccc6637fe2587c5363056d327d97
Jeff Becker <jeff@i2p.rocks>
2021-06-08 08:32:01 -0400

    export functions in liblokinet for win32 dll

--- include/lokinet/lokinet_socket.h
+++ include/lokinet/lokinet_socket.h
@@ -11,11 +11,11 @@ extern "C"
   /// poll many sockets for activity
   /// each pollfd.fd should be set to the socket id
   /// returns 0 on sucess
-  int
+  int EXPORT
   lokinet_poll(struct pollfd* poll, nfds_t numsockets, struct lokinet_context* ctx);
 
   /// close a udp socket or a stream socket by its id
-  void
+  void EXPORT
   lokinet_close_socket(int id, struct lokinet_context* ctx);
 
 #ifdef __cplusplus
