81d27c35c16893368109693fc0029fc8586a4570
Jason Rhinelander <jason@imaginary.ca>
2021-07-29 14:26:12 -0300

    Default CODESIGN_APPEX to CODESIGN_APP
    Sometimes (e.g. dev builds) these can apparently be the same; sometimes
    it seems they need to be different, because Apple.

--- contrib/mac.sh
+++ contrib/mac.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 #
-# build the shit on mac
-# t. jeff
+# Build the shit on mac
+#
+# You will generally need to add: -DCODESIGN_APP=... to make this work, and (unless you are a
+# lokinet team member) will need to pay Apple money for your own team ID and arse around with
+# provisioning profiles.  See macos/README.txt.
 #
 
 set -e
 set +x
+if ! [ -f LICENSE.txt ] || ! [ -d llarp ]; then
+    echo "You need to run this as ./contrib/mac.sh from the top-level lokinet project directory"
+fi
+
 mkdir -p build-mac
 cd build-mac
 cmake \
@@ -25,4 +32,8 @@ cmake \
       -DCMAKE_BUILD_TYPE=Release \
       "$@" \
       ..
-ninja install && ninja sign
+ninja sign
+
+echo -e "Build complete, your app is here:\n"
+ls -lad daemon/lokinet.app
+echo ""
