ce42f1e2dba047c10913bc7808cc3ef96fec1cea
Jeff <jeff@oxen.io>
2022-04-01 11:13:15 -0400

    remove dead json parsing code

--- llarp/util/json.hpp
+++ llarp/util/json.hpp
@@ -1,53 +1,18 @@
 #pragma once
 
 #include <nlohmann/json.hpp>
-
-#include <memory>
-#include <iostream>
 #include <optional>
 
-namespace llarp
+namespace llarp::json
 {
-  namespace json
+  /// maybe get a sub element in a dict by key or return a fallback
+  template <typename T>
+  std::optional<T>
+  maybe_get(
+      const nlohmann::json& obj, std::string_view key, std::optional<T> fallback = std::nullopt)
   {
-    using Object = nlohmann::json;
-
-    struct IParser
-    {
-      virtual ~IParser() = default;
-
-      /// result from feeding data to parser
-      enum Result
-      {
-        /// we need more data to finish parsing
-        eNeedData,
-        /// we have parsed the object fully
-        eDone,
-        /// we have a parsing syntax error
-        eParseError
-      };
-
-      /// feed data to parser return true if successful
-      virtual bool
-      FeedData(const char* buf, size_t sz) = 0;
-      /// parse internal buffer
-      virtual Result
-      Parse(Object& obj) const = 0;
-    };
-
-    /// create new parser
-    IParser*
-    MakeParser(size_t contentSize);
-
-    /// maybe get a sub element in a dict by key or return a fallback
-    template <typename T>
-    std::optional<T>
-    maybe_get(
-        const nlohmann::json& obj, std::string_view data, std::optional<T> fallback = std::nullopt)
-    {
-      if (auto itr = obj.find(data); itr != obj.end())
-        return itr->get<T>();
-      return fallback;
-    }
-  }  // namespace json
-}  // namespace llarp
+    if (auto itr = obj.find(key); itr != obj.end())
+      return itr->get<T>();
+    return fallback;
+  }
+}  // namespace llarp::json
