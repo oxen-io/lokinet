7d188ce8ba3a944e80e690f884f75daa88f910d9
Jeff <jeff@oxen.io>
2022-04-05 17:48:04 -0400

    "more checks" or something

--- pybind/test_lokinet.py
+++ pybind/test_lokinet.py
@@ -5,3 +5,8 @@ from util import run_lokinet
 def test_lokinet_is_fast_af():
     assert run_lokinet(5)
 
+def test_lokinet_is_fast():
+    assert run_lokinet(10)
+
+def test_lokinet_is_pretty_okay_speed():
+    assert run_lokinet(30)
