342763a4d6375e1851781bc2a05f33de65d96ef6
Jeff <jeff@oxen.io>
2022-04-05 17:06:23 -0400

    make util.py main section do many runs and collect statistics

--- pybind/util.py
+++ pybind/util.py
@@ -7,6 +7,7 @@ from threading import Thread
 
 import requests
 import time
+from datetime import datetime
 
 pylokinet.set_net_id("gamma")
 
@@ -42,52 +43,78 @@ class Waiter(Thread):
         if self._finished:
             self._finished(self._work_completed)
 
-def hook(ctx, good):
+def hook(ctx, good, trial, check=True):
     ctx.stop()
-    print(f'test was {"" if good else "no"} bueno')
-    assert good
+    print(f'[{trial}] test was {"" if good else "no "}bueno')
+    if check:
+        assert good
 
-def run_lokinet(wait_for, *, nodedb_class=dict, pin_hops=list(), snode="55fxrybf3jtausbnmxpgwcsz9t8qkf5pr8t5f4xyto4omjrkorpy.snode:35520"):
+def run_lokinet(wait_for, *, trial=0, nodedb=None, pin_hops=list(), target=("55fxrybf3jtausbnmxpgwcsz9t8qkf5pr8t5f4xyto4omjrkorpy.snode", 35520), check=True):
     ctx = Lokinet()
-    waiter = Waiter(wait_for, lambda good: hook(ctx, good))
+    waiter = Waiter(wait_for, lambda good: hook(ctx, good, trial, check))
     waiter.start()
     # pin first hops
     if pin_hops:
         for hop in pin_hops:
             ctx.set_config_opt('network', 'strict-connect', hop)
         ctx.set_config_opt('router', 'min-routers', f'len(pin_hops)')
-    ctx.set_config_opt('network', 'reachable', 'false')
+        ctx.set_config_opt('router', 'max-routers', f'len(pin_hops)')
+    #ctx.set_config_opt('network', 'reachable', 'false')
     ctx.set_config_opt('api', 'enabled', 'false')
-    ctx.set_config_opt('logging', 'level', 'info')
+    ctx.set_config_opt('logging', 'level', 'none')
     ctx.set_config_opt('network', 'profiling', 'false')
-    db = None
-    if nodedb_class:
-        db = nodedb_class()
-        ctx.nodedb_load = lambda : db
-        ctx.nodedb_store = db.__setitem__
-        ctx.nodedb_del = db.__delitem__
+
+    if nodedb is not None:
+        print(f'[{trial}] set up nodedb with {len(nodedb)} nodes')
+        ctx.nodedb_load = lambda : dict(nodedb)
+        ctx.nodedb_store = nodedb.__setitem__
+        ctx.nodedb_del = nodedb.__delitem__
     req = requests.get("https://seed.lokinet.org/testnet.signed", stream=True)
     ctx.add_bootstrap_rc(req.content)
-    print("starting....")
+    print(f"[{trial}] starting....")
     ctx.start()
     while not ctx.wait_for_ready(100):
         assert waiter.working
-    print(f"we are {ctx.localaddr()}")
+    print(f"[{trial}] we are {ctx.localaddr()}")
     id = None
+    success = False
     try:
-        addr, port, id = ctx.resolve(snode, wait_for)
-        print(f"resolved {snode} as {addr}:{port} on {id}")
-        resp = requests.get(f"https://{addr}:{port}/", verify=False)
-        print(resp.text)
+        print(f'[{trial}] trying to resolve {target[0]}')
+        addr, port, id = ctx.resolve(f'{target[0]}:{target[1]}', 5)
+        print(f"[{trial}] resolved {target[0]} as {addr}:{port} on {id}")
+        resp = requests.get(f"http://{addr}:{port}/", headers={'host': target[0]})
+        assert resp.status_code < 300
+        success = True
     except Exception as ex:
-        print(f'failed: {ex}')
+        print(f'[{trial}] failed: {ex}')
+        success = False
     finally:
         if id:
             ctx.unresolve(id)
     waiter.done()
     del ctx
-    print(f'we have {len(db)} nodedb entries left over')
-    print(f'{list(db.keys())}')
+    if nodedb:
+        print(f'[{trial}] we have {len(nodedb)} nodedb entries left over')
+        #print(f'[{trial}] {list(nodedb.keys())}')
+    return success
 
 if __name__ == '__main__':
-    run_lokinet(30)
+    db = dict()
+    fails = 0
+    errors = 0
+    good = 0
+    times = 30
+    timeout = 15
+    pylokinet.set_log_level('none')
+    for n in range(times):
+        try:
+            if run_lokinet(timeout, target=('blocks.loki', 80), nodedb=db, check=False, trial=f'{datetime.now()} | test {1+n}'):
+                good += 1
+            else:
+                fails += 1
+        except:
+            fails += 1
+            errors += 1
+        time.sleep(1)
+    print(f'started up {times-errors} of {times} ({float(float(times-errors) / times) * 100}%)')
+    print(f'succeeded {times-fails} of {times} ({float(float(times-fails) / times) * 100}%)')
