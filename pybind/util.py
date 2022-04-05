5d5dc93d4f2add1fe47d5b24702bdeb229ca4c2d
Jeff <jeff@oxen.io>
2022-04-05 10:58:08 -0400

    pytest fixes
    * fail tests if they take too long

--- pybind/util.py
+++ pybind/util.py
@@ -3,11 +3,12 @@
 import pylokinet
 from pylokinet import Context as Lokinet
 
-pylokinet.set_net_id("gamma")
-
-import time
+from threading import Thread
 
 import requests
+import time
+
+pylokinet.set_net_id("gamma")
 
 def load_nodedb():
     return dict()
@@ -18,15 +19,36 @@ def store_nodedb(k, v):
 def del_nodedb_entry(k):
     print(f'delete entry from nodedb: {k}')
 
+class Waiter(Thread):
+
+    def __init__(self, wait_for=1000, finish=None):
+        Thread.__init__(self)
+        self._wait_for = wait_for
+        self._work_completed = False
+        self._finished = finish
+
+    def done(self):
+        self._work_completed = True
+
+    def run(self):
+        time.sleep(self._wait_for)
+        if self._finished:
+            self._finished(self._work_completed)
+
+def _ended(ctx, good):
+    ctx.stop()
+    assert good
+
 def lokinet_run(wait_for, snode="55fxnzi5myfm1ypnx5w4c5714jgo15tks45dgf6isgut9onnduxo.snode:35515"):
-    endat = time.time() + wait_for
     ctx = Lokinet()
+    waiter = Waiter(wait_for, lambda good: _ended(ctx, good))
+    waiter.start()
     ctx.set_config_opt('network', 'hops', '2')
     ctx.set_config_opt('network', 'reachable', 'false')
     ctx.set_config_opt('api', 'enabled', 'false')
     ctx.set_config_opt('logging', 'level', 'trace')
     ctx.set_config_opt('network', 'profiling', 'false')
-     
+
     ctx.nodedb_load = load_nodedb
     ctx.nodedb_store = store_nodedb
     ctx.nodedb_del = del_nodedb_entry
@@ -35,7 +57,7 @@ def lokinet_run(wait_for, snode="55fxnzi5myfm1ypnx5w4c5714jgo15tks45dgf6isgut9on
     print("starting....")
     ctx.start()
     while not ctx.wait_for_ready(100):
-        assert endat >= time.time()
+        pass
     print(f"we are {ctx.localaddr()}")
     id = None
     try:
@@ -46,6 +68,7 @@ def lokinet_run(wait_for, snode="55fxnzi5myfm1ypnx5w4c5714jgo15tks45dgf6isgut9on
     finally:
         if id:
             ctx.unresolve(id)
+    waiter.done()
 
 if __name__ == '__main__':
-    lokinet_run(5)
+    lokinet_run(30)
