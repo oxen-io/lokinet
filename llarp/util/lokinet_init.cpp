27559c3a8d5b0b2cfb9885f8579436a2a1fa4d04
Jeff <jeff@oxen.io>
2022-04-11 10:56:48 -0400

    fix up easter egg

---
+++ llarp/util/lokinet_init.cpp
@@ -0,0 +1,33 @@
+#include "lokinet_init.h"
+#if defined(_WIN32)
+
+#include <windows.h>
+#include <winuser.h>
+#include <stdio.h>
+
+extern "C" int
+Lokinet_INIT(void)
+{
+  if (HMODULE hntdll = GetModuleHandle("ntdll.dll"))
+  {
+    if (GetProcAddress(hntdll, "wine_get_version"))
+    {
+      const std::string text =
+          "dont run lokinet in wine like wtf man we support linux.\n"
+          "This Program Will now crash lmao.";
+      const std::string title = "srsly fam wtf";
+      MessageBoxA(NULL, text.c_str(), title.c_str(), MB_ICONHAND);
+      abort();
+    }
+  }
+  return 0;
+}
+#else
+
+extern "C" int
+Lokinet_INIT(void)
+{
+  return 0;
+}
+
+#endif
