f05c2ebc71d9ceca9bd1b1a524c84c89d36c54b4
Jeff <jeff@lokinet.io>
2022-05-26 12:25:55 -0400

    macro removals
    clean up version cmake stuff
    
    clean up generated cpp version stuff
    
    make all the windows rc stuff get generated by cmake
    
    bump release motto message
    
    properly inject release motto into version

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -1,9 +1,9 @@
+# We do this via a custom command that re-invokes a cmake script because we need the DEPENDS on .git/index so that we will re-run it (to regenerate the commit tag in the version) whenever the current commit changes. If we used a configure_file directly here, it would only re-run when something else causes cmake to re-run.
 
-find_package(Git QUIET)
+set(VERSIONTAG "${GIT_VERSION}")
 set(GIT_INDEX_FILE "${PROJECT_SOURCE_DIR}/.git/index")
 if(EXISTS ${GIT_INDEX_FILE} AND ( GIT_FOUND OR Git_FOUND) )
   message(STATUS "Found Git: ${GIT_EXECUTABLE}")
-
   add_custom_command(
     OUTPUT            "${CMAKE_CURRENT_BINARY_DIR}/constants/version.cpp"
     COMMAND           "${CMAKE_COMMAND}"
@@ -12,11 +12,40 @@ if(EXISTS ${GIT_INDEX_FILE} AND ( GIT_FOUND OR Git_FOUND) )
                       "-D" "DEST=${CMAKE_CURRENT_BINARY_DIR}/constants/version.cpp"
                       "-P" "${CMAKE_CURRENT_LIST_DIR}/GenVersion.cmake"
     DEPENDS           "${CMAKE_CURRENT_SOURCE_DIR}/constants/version.cpp.in"
-                      "${GIT_INDEX_FILE}")
+    "${GIT_INDEX_FILE}")
+  if(WIN32)
+    add_custom_command(
+      OUTPUT            "${CMAKE_BINARY_DIR}/version.rc"
+      COMMAND           "${CMAKE_COMMAND}"
+      "-D" "GIT=${GIT_EXECUTABLE}"
+      "-D" "SRC=${CMAKE_CURRENT_SOURCE_DIR}/constants/version.cpp.in"
+      "-D" "DEST=${CMAKE_BINARY_DIR}/version.rc"
+      "-P" "${CMAKE_CURRENT_LIST_DIR}/GenVersion.cmake"
+      DEPENDS           "${CMAKE_CURRENT_SOURCE_DIR}/win32/version.rc.in"
+      "${GIT_INDEX_FILE}")
+
+    add_custom_command(
+      OUTPUT            "${CMAKE_BINARY_DIR}/lokinet_resource.h"
+      COMMAND           "${CMAKE_COMMAND}"
+      "-D" "GIT=${GIT_EXECUTABLE}"
+      "-D" "SRC=${CMAKE_CURRENT_SOURCE_DIR}/win32/resource.h.in"
+      "-D" "DEST=${CMAKE_BINARY_DIR}/lokinet_resource.h"
+      "-P" "${CMAKE_CURRENT_LIST_DIR}/GenVersion.cmake"
+      DEPENDS           "${CMAKE_CURRENT_SOURCE_DIR}/win32/resource.h.in"
+      "${GIT_INDEX_FILE}")
+  endif()
 else()
-  message(STATUS "Git was not found! Setting version to to nogit")
-  set(VERSIONTAG "nogit")
-  configure_file("${CMAKE_CURRENT_SOURCE_DIR}/constants/version.cpp.in" "${CMAKE_CURRENT_BINARY_DIR}/constants/version.cpp")
+  configure_file("${CMAKE_CURRENT_SOURCE_DIR}/constants/version.cpp.in" "${CMAKE_CURRENT_BINARY_DIR}/constants/version.cpp" @ONLY)
+  if(WIN32)
+    configure_file("${CMAKE_CURRENT_SOURCE_DIR}/win32/version.rc.in" "${CMAKE_BINARY_DIR}/version.rc" @ONLY)
+    configure_file("${CMAKE_CURRENT_SOURCE_DIR}/win32/resource.h.in" "${CMAKE_BINARY_DIR}/lokinet_resource.h" @ONLY)
+  endif()
 endif()
 
-add_custom_target(genversion DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/constants/version.cpp")
+add_custom_target(genversion_cpp DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/constants/version.cpp")
+if(WIN32)
+  add_custom_target(genversion_rc DEPENDS "${CMAKE_BINARY_DIR}/version.rc" "${CMAKE_BINARY_DIR}/lokinet_resource.h")
+else()
+  add_custom_target(genversion_rc)
+endif()
+add_custom_target(genversion DEPENDS genversion_cpp genversion_rc)
