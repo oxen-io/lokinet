e480e36f3de45ee50bfe504cf32e26ec5c52cc48
Jeff <jeff@lokinet.io>
2022-05-27 10:14:31 -0400

    generate windows rc for each executable target so we can set the executable name right for each one

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -12,39 +12,34 @@ if(EXISTS ${GIT_INDEX_FILE} AND ( GIT_FOUND OR Git_FOUND) )
                       "-D" "DEST=${CMAKE_CURRENT_BINARY_DIR}/constants/version.cpp"
                       "-P" "${CMAKE_CURRENT_LIST_DIR}/GenVersion.cmake"
     DEPENDS           "${CMAKE_CURRENT_SOURCE_DIR}/constants/version.cpp.in"
-    "${GIT_INDEX_FILE}")
+                      "${GIT_INDEX_FILE}")
   if(WIN32)
-    add_custom_command(
-      OUTPUT            "${CMAKE_BINARY_DIR}/version.rc"
-      COMMAND           "${CMAKE_COMMAND}"
-      "-D" "GIT=${GIT_EXECUTABLE}"
-      "-D" "SRC=${CMAKE_CURRENT_SOURCE_DIR}/constants/version.cpp.in"
-      "-D" "DEST=${CMAKE_BINARY_DIR}/version.rc"
-      "-P" "${CMAKE_CURRENT_LIST_DIR}/GenVersion.cmake"
-      DEPENDS           "${CMAKE_CURRENT_SOURCE_DIR}/win32/version.rc.in"
-      "${GIT_INDEX_FILE}")
-
-    add_custom_command(
-      OUTPUT            "${CMAKE_BINARY_DIR}/lokinet_resource.h"
-      COMMAND           "${CMAKE_COMMAND}"
-      "-D" "GIT=${GIT_EXECUTABLE}"
-      "-D" "SRC=${CMAKE_CURRENT_SOURCE_DIR}/win32/resource.h.in"
-      "-D" "DEST=${CMAKE_BINARY_DIR}/lokinet_resource.h"
-      "-P" "${CMAKE_CURRENT_LIST_DIR}/GenVersion.cmake"
-      DEPENDS           "${CMAKE_CURRENT_SOURCE_DIR}/win32/resource.h.in"
-      "${GIT_INDEX_FILE}")
+    foreach(exe IN ITEMS lokinet lokinet-vpn lokinet-bootstrap)
+      set(lokinet_EXE_NAME "${exe}.exe")
+      add_custom_command(
+        OUTPUT            "${CMAKE_BINARY_DIR}/${exe}.rc"
+        COMMAND           "${CMAKE_COMMAND}"
+        "-D" "GIT=${GIT_EXECUTABLE}"
+        "-D" "SRC=${CMAKE_CURRENT_SOURCE_DIR}/win32/version.rc.in"
+        "-D" "DEST=${CMAKE_BINARY_DIR}/${exe}.rc"
+        "-P" "${CMAKE_CURRENT_LIST_DIR}/GenVersion.cmake"
+        DEPENDS           "${CMAKE_CURRENT_SOURCE_DIR}/win32/version.rc.in"
+        "${GIT_INDEX_FILE}")
+    endforeach()
   endif()
 else()
   configure_file("${CMAKE_CURRENT_SOURCE_DIR}/constants/version.cpp.in" "${CMAKE_CURRENT_BINARY_DIR}/constants/version.cpp" @ONLY)
   if(WIN32)
-    configure_file("${CMAKE_CURRENT_SOURCE_DIR}/win32/version.rc.in" "${CMAKE_BINARY_DIR}/version.rc" @ONLY)
-    configure_file("${CMAKE_CURRENT_SOURCE_DIR}/win32/resource.h.in" "${CMAKE_BINARY_DIR}/lokinet_resource.h" @ONLY)
+    foreach(exe IN ITEMS lokinet lokinet-vpn lokinet-bootstrap)
+      set(lokinet_EXE_NAME "${exe}.exe")
+      configure_file("${CMAKE_CURRENT_SOURCE_DIR}/win32/version.rc.in" "${CMAKE_BINARY_DIR}/${exe}.rc" @ONLY)
+    endforeach()
   endif()
 endif()
 
 add_custom_target(genversion_cpp DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/constants/version.cpp")
 if(WIN32)
-  add_custom_target(genversion_rc DEPENDS "${CMAKE_BINARY_DIR}/version.rc" "${CMAKE_BINARY_DIR}/lokinet_resource.h")
+  add_custom_target(genversion_rc DEPENDS "${CMAKE_BINARY_DIR}/lokinet.rc" "${CMAKE_BINARY_DIR}/lokinet-vpn.rc" "${CMAKE_BINARY_DIR}/lokinet-bootstrap.rc")
 else()
   add_custom_target(genversion_rc)
 endif()
