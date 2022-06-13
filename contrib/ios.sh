18c3d8bd2ed431194cf3facb95a8e6ecc09abc46
jeff <jeff@oxen.io>
2022-06-13 12:58:17 -0400

    initial ios junk

---
+++ contrib/ios.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Build the shit for iphone, only builds embeded lokinet
+
+set -e
+set +x
+if ! [ -f LICENSE ] || ! [ -d llarp ]; then
+    echo "You need to run this as ./contrib/ios.sh from the top-level lokinet project directory"
+fi
+
+mkdir -p build/iphone
+cd build/iphone
+cmake \
+    -G Ninja \
+    -DCMAKE_TOOLCHAIN_FILE=../../external/iso-cmake/ios.toolchain.cmake \
+    -DBUILD_STATIC_DEPS=ON \
+    -DBUILD_PACKAGE=OFF \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DBUILD_TESTING=OFF \
+    -DBUILD_LIBLOKINET=ON \
+    -DWITH_TESTS=OFF \
+    -DNATIVE_BUILD=OFF \
+    -DSTATIC_LINK=ON \
+    -DWITH_SYSTEMD=OFF \
+    -DFORCE_OXENMQ_SUBMODULE=ON \
+    -DFORCE_OXENC_SUBMODULE=ON \
+    -DSUBMODULE_CHECK=ON \
+    -DWITH_LTO=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    "$@" \
+    ../..
+ninja lokinet-shared
