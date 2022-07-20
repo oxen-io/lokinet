f222aecc79a3eada282d010752d6a20d5666fcca
Jeff <jeff@lokinet.io>
2022-07-20 15:45:09 -0400

    actually use correct variable in iteration

--- llarp/net/net.cpp
+++ llarp/net/net.cpp
@@ -307,7 +307,7 @@ namespace llarp::net
       if (getifaddrs(&addrs))
         throw std::runtime_error{fmt::format("getifaddrs(): {}", strerror(errno))};
 
-      for (auto next = addrs; addrs and addrs->ifa_next; addrs = addrs->ifa_next)
+      for (auto next = addrs; next and next->ifa_next; next = next->ifa_next)
         visit(next);
 
       freeifaddrs(addrs);
