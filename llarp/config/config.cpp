dd16158081777c0402412912d7da71cad202982b
Jason Rhinelander <jason@imaginary.ca>
2022-09-09 23:44:21 -0300

    DNS: default to 127.3.2.1 & high port on Linux
    Lots of tools struggle with non-default DNS port, so keep a listener on
    127.3.2.1:53 (by default).
    
    This required various changes to the config handling to hold a vector
    (instead of an optional) of defaults and values, and now allows passing
    in an array of defaults instead of just a single default.

--- llarp/config/config.cpp
+++ llarp/config/config.cpp
@@ -776,17 +776,18 @@ namespace llarp
     // Most non-linux platforms have loopback as 127.0.0.1/32, but linux uses 127.0.0.1/8 so that we
     // can bind to other 127.* IPs to avoid conflicting with something else that may be listening on
     // 127.0.0.1:53.
+    constexpr std::array DefaultDNSBind{
 #ifdef __linux__
 #ifdef WITH_SYSTEMD
-    // when we have systemd support add a random high port on loopback
-    // see https://github.com/oxen-io/lokinet/issues/1887#issuecomment-1091897282
-    constexpr Default DefaultDNSBind{"127.0.0.1:0"};
-#else
-    constexpr Default DefaultDNSBind{"127.3.2.1:53"};
+        // when we have systemd support add a random high port on loopback as well
+        // see https://github.com/oxen-io/lokinet/issues/1887#issuecomment-1091897282
+        Default{"127.0.0.1:0"},
 #endif
+        Default{"127.3.2.1:53"},
 #else
-    constexpr Default DefaultDNSBind{"127.0.0.1:53"};
+        Default{"127.0.0.1:53"},
 #endif
+    };
 
     // Default, but if we get any upstream (including upstream=, i.e. empty string) we clear it
     constexpr Default DefaultUpstreamDNS{"9.9.9.10:53"};
@@ -829,9 +830,10 @@ namespace llarp
         "query-bind",
 #ifdef __APPLE__
         Default{"127.0.0.1:1253"},
-#endif
-#ifdef _WIN32
+#elif defined(_WIN32)
         Default{"0.0.0.0:0"},
+#else
+        Hidden,
 #endif
         Comment{
             "Address to bind to for sending upstream DNS requests.",
@@ -842,6 +844,7 @@ namespace llarp
         "dns",
         "bind",
         DefaultDNSBind,
+        MultiValue,
         Comment{
             "Address to bind to for handling DNS requests.",
         },
