4ad66ac2a55275e5e2447e31a8bc101feb83795b
Jason Rhinelander <jason@imaginary.ca>
2022-10-28 21:46:06 -0300

    Remove unused VERSION_STR constant

--- llarp/constants/version.hpp
+++ llarp/constants/version.hpp
@@ -8,7 +8,6 @@ namespace llarp
   // Given a full lokinet version of: lokinet-1.2.3-abc these are:
   extern const std::array<uint16_t, 3> VERSION;         // [1, 2, 3]
   extern const std::array<uint64_t, 4> ROUTER_VERSION;  // [proto, 1, 2, 3]
-  extern const char* const VERSION_STR;                 // "1.2.3"
   extern const char* const VERSION_TAG;                 // "abc"
   extern const char* const VERSION_FULL;                // "lokinet-1.2.3-abc"
 
