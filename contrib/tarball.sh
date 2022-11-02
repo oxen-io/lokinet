88b5a84df361f5d8933ff66a850c9144766c9a6c
Jeff Becker <jeff@lokinet.io>
2022-11-02 09:24:19 -0400

    explicitly select signing key used in source tarball signing script

--- contrib/tarball.sh
+++ contrib/tarball.sh
@@ -5,4 +5,4 @@
 repo=$(readlink -e $(dirname $0)/..)
 branch=$(test -e $repo/.git/ && git rev-parse --abbrev-ref HEAD)
 out="lokinet-$(git describe --exact-match --tags $(git log -n1 --pretty='%h') 2> /dev/null || ( echo -n $branch- && git rev-parse --short HEAD)).tar.xz"
-git-archive-all -C $repo --force-submodules $out && rm -f $out.sig && (gpg --sign --detach $out &> /dev/null && gpg --verify $out.sig)
+git-archive-all -C $repo --force-submodules $out && rm -f $out.sig && (gpg -u jeff@lokinet.io --sign --detach $out &> /dev/null && gpg --verify $out.sig)
