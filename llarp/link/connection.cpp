dbad0d596aad0bbc18fb057ebd0c1de1d13eef20
dr7ana <danielrouhana@gmail.com>
2023-12-13 05:49:59 -0800

    Fixed pending message queue weirdness

--- llarp/link/connection.cpp
+++ llarp/link/connection.cpp
@@ -5,7 +5,7 @@ namespace llarp::link
   Connection::Connection(
       const std::shared_ptr<oxen::quic::connection_interface>& c,
       std::shared_ptr<oxen::quic::BTRequestStream>& s)
-      : conn{c}, control_stream{s}/* , remote_rc{std::move(rc)} */
+      : conn{c}, control_stream{s} /* , remote_rc{std::move(rc)} */
   {}
 
 }  // namespace llarp::link
